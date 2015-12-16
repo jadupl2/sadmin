7667e533f02c075de5b35686ec8dd6e6e924f100
Jacques Duplessis <duplessis.jacques@gmail.com>
2015-12-16 16:03:53 -0500

    Now mu .gitignore file works correctly

--- jac/ref/svn_delete_project.sh
+++
@@ -1,8 +0,0 @@
-#set -x
-PROJECT=$1                              ; export PROJECT
-export BASE_DIR="/svn/sadmin"           ; export BASE_DIR
-WDATE=`date`				; export WDATE
-svn delete -m "Delete Project" file:///${BASE_DIR}/${PROJECT}
-
-
-# However, if you need to delete all record of it in the repo, use another method. 
--- jac/ref/test_ip.py
+++
@@ -1,5 +0,0 @@
-import ipcalc
-ip='192.168.1.1'
-subnet='25'
-for x in ipcalc.Network(ip+'/'+subnet):
-    print str(x)
