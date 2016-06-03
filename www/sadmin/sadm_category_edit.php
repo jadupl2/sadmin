5b8d18e35b456b0e4302ac1b65d0830113dc7387
Jacques Duplessis <jacques@holmes.maison.ca>
2016-06-03 10:59:18 -0400

    Category edit working

--- www/sadmin/sadm_category_edit.php
+++ www/sadmin/sadm_category_edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 * ==================================================================================================
 *   Author   :  Jacques Duplessis
@@ -21,7 +21,7 @@
 *   If not, see <http://www.gnu.org/licenses/>.
 * ==================================================================================================
 */
-require_once      ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_constants.php'); 
+require_once      ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_constants.php');
 require_once      ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_connect.php');
 require_once      ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_lib.php');
 require_once      ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_header.php');
@@ -31,15 +31,15 @@ require_once      ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_header.php');
 #                                       Local Variables
 #===================================================================================================
 #
-$DEBUG = True;                                       # Activate (TRUE) or Deactivate (FALSE) Debug
+$DEBUG = False;                                       # Activate (TRUE) or Deactivate (FALSE) Debug
 
-        
+
 // ================================================================================================
 //                      Display Category Data used in the data input form
 // ================================================================================================
 function display_record( $wrow , $title) {
     sadm_page_heading ("$title");                                       # Display Page Title
-    
+
 
     echo "<br><center>\n";
     #echo '<table frame="border" border="1" cellspacing="0" width="90%" >';
@@ -47,10 +47,10 @@ function display_record( $wrow , $title) {
 
     $BGCOLOR="blue" ; $FNT_COLOR="#FFFFFF";
     $BGCOLOR="lavender";
-    
-    // Table Key - Category Code 
+
+    // Table Key - Category Code
     echo "<tr>\n";
-    echo "<td bgcolor=#4CAF50  align=left><b>Category Code : " . $wrow['cat_code'] ;
+    echo "<td><input type='text' name='scr_code' size=10 value='" .$wrow['cat_code']. "'readonly>\n";
     echo "</td>\n";
     echo "</tr>\n";
     echo "</table>\n";
@@ -58,12 +58,13 @@ function display_record( $wrow , $title) {
 
     echo '<table frame="border" border="1" cellspacing="0" width="90%" >';
 
-    // Category Description  
-    echo "<tr><td  bgcolor=$BGCOLOR><b>Category Description</b></td>";
-    echo "<td bgcolor=$BGCOLOR><input type='text' name='scr_desc' size=30 value='" .
-        $wrow['cat_desc'] . "' /></td>";
+    // Category Description
+    echo "<tr>\n";
+    echo "<td bgcolor=$BGCOLOR><b>Category Description</b></td>\n";
+    echo "<td><input type='text' name='scr_desc' size=30 value='" .$wrow['cat_desc']. "'/></td>\n";
+    echo "</tr>\n";
 
- 
+
     // Category Status
     echo "<tr>\n";
     echo "<td bgcolor=$BGCOLOR><b>Category Status</b></td>";
@@ -79,7 +80,7 @@ function display_record( $wrow , $title) {
     echo "</tr>\n";
     echo "</table>\n";
 }
-    
+
 
 /*
 * ==================================================================================================
@@ -87,49 +88,59 @@ function display_record( $wrow , $title) {
 * ==================================================================================================
 */
 
-    if (isset($_GET['sel']) ) { 
-        $wkey = $_GET['sel'];
-        if ($DEBUG) { echo "<br>Category received is " . $wkey; }       # Under Debug Show Cat Rcv. 
         if (isset($_POST['submitted'])) {
             if ($DEBUG) { echo "<br>Post Submitted for " . $scr_code; } # Under Debug Show Cat Code
-            #foreach($_POST AS $key => $value) { $_POST[$key] = $value; }
-            #$var = $_POST;
-            #$firephp->log($var, 'Post');
-            if ($DEBUG) { echo "<br>scr_desc = $_POST['scr_desc']"; }     
-            $sql = "UPDATE sadm.category SET cat_code = '" . $wkey . "',
-                cat_desc           =  '" . $_POST['scr_desc'] . "',
-                cat_status         =  '" . $_POST['scr_status'] ."',
-                WHERE cat_code = '" . $wkey . "';"; 
-            if ($DEBUG) { echo "<br>Update SQL = $sql"; }     
-            $row = pg_query($query) or die('Query failed: ' . pg_last_error());
-            header("location:/sadmin/sadm_category_main.php");
+            foreach($_POST AS $key => $value) { $_POST[$key] = $value; }
+
+            if ($DEBUG) { echo "<br>scr_desc = " .  $_POST['scr_desc'] ; }
+            $sql1 = "UPDATE sadm.category SET ";
+            $sql2 = "cat_code       = '" . $_POST['scr_code']   ."', ";
+            $sql3 = "cat_desc       = '" . $_POST['scr_desc']   ."', ";
+            $sql4 = "cat_status     = '" . $_POST['scr_status'] ."'  ";
+            $sql5 = "WHERE cat_code = '" . $_POST['scr_code']   ."'; ";
+            $sql  = $sql1 . $sql2 . $sql3 . $sql4 . $sql5 ;
+            if ($DEBUG) { echo "<br>Update SQL = $sql"; }
+            #$row = pg_query($sql) or die('Query failed: ' . pg_last_error());
+            $row = pg_query($sql) ;
+            if (!$row){  
+                echo "<br>Update failed!!";  
+                echo "<br>Problem with Command :" . $sql ;
+                echo "<br>pg_last_error()"  ;
+            }else{  
+                echo "<br>Update successfull<br>";  
+            }  
+            echo "<a href=http://sadmin/sadmin/sadm_category_main.php>Return to Categories List</a>";
             exit;
         }else{
-            if ($DEBUG) { echo "<br>Post is not Submitted - Read Category " . $wkey; } 
-            $query = "SELECT * FROM sadm.category WHERE cat_code = '" . $wkey ."';";
-            if ($DEBUG) { echo "<br>SQL = $query"; } 
-            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-            if (!$result) {
-                echo "An error while running SQL Query occurred.\n";
-                exit;
+            if (isset($_GET['sel']) ) {
+                $wkey = $_GET['sel'];
+                if ($DEBUG) { echo "<br>Category received is " . $wkey; }       # Under Debug Show Cat Rcv.
+                if ($DEBUG) { echo "<br>Post is not Submitted - Read Category " . $wkey; }
+                $query = "SELECT * FROM sadm.category WHERE cat_code = '" . $wkey ."';";
+                if ($DEBUG) { echo "<br>SQL = $query"; }
+                $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                if (!$result) {
+                    echo "An error while running SQL Query occurred.\n";
+                    exit;
+                }else{
+                    $row = pg_fetch_array($result, null, PGSQL_ASSOC) ;
+
+                }
             }else{
-                $row = pg_fetch_array($result, null, PGSQL_ASSOC) ;
-                display_record( $row , "Edit Category Information");
-            }   
+                echo "<br>No Selection Received - Please Advice" ;
+                exit ;
+            }
         }
-    }else{
-         echo "<br>No Selection Received - Please Advice" ;
-         exit ;
-    }
 ?>
 
-    <form action='' method='POST'>
-        <p><input type='submit' value='Save' />
-        <input type='hidden' value='1' name='submitted' /> 
+    <form name="cat_edit" action="<?php echo htmlentities($_SERVER['PHP_SELF']) ?>" method='POST'>
+        <br>
+        <?php display_record( $row , "Edit Category Information"); ?>
+        <input type='submit' value='Save' />
+        <input type='hidden' value='1' name='submitted' />
     </form>
     <a href="/sadmin/sadm_category_main.php"><button name="cancel" value="0" type="button">Cancel</button></a>
 
 <?php
      include ($_SERVER['DOCUMENT_ROOT'].'/lib/sadm_footer.php')  ;       # SADM Std EndOfPage Footer
 ?>
-
