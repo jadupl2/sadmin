5aa8e18d7d64f9d6ad36146555e56eea93e870f3
Jacques Duplessis <duplessis.jacques@gmail.com>
2017-02-16 09:05:03 -0500

    New Command LIne Alert Viewer (sadm_sysmon_cli.sh) - smon is an alias to viewer

---
+++ bin/sadm_sysmon_cli.sh
@@ -0,0 +1,98 @@
+#! /usr/bin/env bash
+# --------------------------------------------------------------------------------------------------
+#   Author   :  Jacques Duplessis
+#   Title    :  sadm_sysmon_cli.sh
+#   Synopsis :  Run the SADM System Monitor (sadm_sysmon.pl) and display Result file (hostname.rpt)
+#   Version  :  1.6
+#   Date     :  14 Fevrier 2017
+#   Requires :  sh
+#
+#   Copyright (C) 2016 Jacques Duplessis <duplessis.jacques@gmail.com>
+#
+#   The SADMIN Tool is free software; you can redistribute it and/or modify it under the terms
+#   of the GNU General Public License as published by the Free Software Foundation; either
+#   version 2 of the License, or (at your option) any later version.
+
+#   SADMIN Tools are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program.
+#   If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------------------------
+# Enhancements/Corrections Version Log
+# 1.7   
+# 
+# --------------------------------------------------------------------------------------------------
+trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPTE LE ^C
+#set -x
+
+
+#===================================================================================================
+# If You want to use the SADMIN Libraries, you need to add this section at the top of your script
+#   Please refer to the file $sadm_base_dir/lib/sadm_lib_std.txt for a description of each
+#   variables and functions available to you when using the SADMIN functions Library
+# --------------------------------------------------------------------------------------------------
+# Global variables used by the SADMIN Libraries - Some influence the behavior of function in Library
+# These variables need to be defined prior to load the SADMIN function Libraries
+# --------------------------------------------------------------------------------------------------
+SADM_PN=${0##*/}                           ; export SADM_PN             # Script name
+SADM_VER='1.6'                             ; export SADM_VER            # Script Version
+SADM_INST=`echo "$SADM_PN" |cut -d'.' -f1` ; export SADM_INST           # Script name without ext.
+SADM_TPID="$$"                             ; export SADM_TPID           # Script PID
+SADM_EXIT_CODE=0                           ; export SADM_EXIT_CODE      # Script Exit Return Code
+SADM_BASE_DIR=${SADMIN:="/sadmin"}         ; export SADM_BASE_DIR       # SADMIN Root Base Dir.
+SADM_LOG_TYPE="L"                          ; export SADM_LOG_TYPE       # 4Logger S=Scr L=Log B=Both
+SADM_LOG_APPEND="N"                        ; export SADM_LOG_APPEND     # Append to Existing Log ?
+SADM_MULTIPLE_EXEC="Y"                     ; export SADM_MULTIPLE_EXEC  # Run many copy at same time
+#
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadm_lib_std.sh     
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_server.sh ] && . ${SADM_BASE_DIR}/lib/sadm_lib_server.sh  
+
+# These variables are defined in sadmin.cfg file - You can also change them on a per script basis 
+SADM_SSH_CMD="${SADM_SSH} -qnp ${SADM_SSH_PORT}" ; export SADM_SSH_CMD  # SSH Command to Access Farm
+SADM_MAIL_TYPE=1                           ; export SADM_MAIL_TYPE      # 0=No 1=Err 2=Succes 3=All
+#SADM_MAX_LOGLINE=5000                       ; export SADM_MAX_LOGLINE   # Max Nb. Lines in LOG )
+#SADM_MAX_RCLINE=100                         ; export SADM_MAX_RCLINE    # Max Nb. Lines in RCH file
+#SADM_MAIL_ADDR="your_email@domain.com"      ; export ADM_MAIL_ADDR      # Email Address of owner
+#===================================================================================================
+#
+
+
+
+# --------------------------------------------------------------------------------------------------
+#                               This Script environment variables
+# --------------------------------------------------------------------------------------------------
+DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDebug Higher=+Verbose
+
+
+
+# --------------------------------------------------------------------------------------------------
+#                                Script Start HERE
+# --------------------------------------------------------------------------------------------------
+    sadm_start                                                          # Init Env Dir & RC/Log File
+    if ! $(sadm_is_root)                                                # Is it root running script?
+        then sadm_writelog "Script can only be run by the 'root' user"  # Advise User Message
+             sadm_writelog "Process aborted"                            # Abort advise message
+             sadm_stop 1                                                # Close and Trim Log
+             exit 1                                                     # Exit To O/S
+    fi
+
+    tput clear 
+    $SADM_BIN_DIR/sadm_sysmon.pl
+    SADM_EXIT_CODE=$?                                                   # Save Process Exit Code
+
+    tput clear 
+    echo "----------------------------------------------------------------------------------"
+    echo "Based on the current configutation file $SADM_CFG_DIR/${HOSTNAME}.cfg"
+    echo "Here is the output file $SADM_RPT_DIR/${HOSTNAME}.rpt of SADM System Monitor"
+    echo "----------------------------------------------------------------------------------"
+    echo " "
+    cat $SADM_RPT_DIR/${HOSTNAME}.rpt
+    echo " "
+    echo "----------------------------------------------------------------------------------"
+    echo " "
+
+    # Go Write Log Footer - Send email if needed - Trim the Log - Update the Recode History File
+    sadm_stop $SADM_EXIT_CODE                                           # Upd. RCH File & Trim Log 
+    exit $SADM_EXIT_CODE                                                # Exit With Global Err (0/1)
