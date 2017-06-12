acb4704fdcb281d740e1447b77288f872801ebc9
Jacques Duplessis <duplessis.jacques@gmail.com>
2017-06-12 09:53:48 -0400

    V2.3 Rewritten Startup & Shutdown Scripts

--- sys/sadm_shutdown.sh
+++ sys/sadm_shutdown.sh
@@ -6,35 +6,62 @@
 #   Synopsis:   This script is run when the sadmin.service is shutdown when server goes down.
 #               Called by /etc/systemd/system/sadmin.service
 # --------------------------------------------------------------------------------------------------
+# Version 2.2 - June 2017
+#               Log Enhancement
+# Version 2.3 - June 2017
+#               Restructure to use the SADM Library and Send email on execution.
+# --------------------------------------------------------------------------------------------------
+trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPTE LE ^C
 #set -x
-#
-# Version 2.2 - June 2017 - Log Enhancement
-#
-#
-#
+
+#===================================================================================================
+# If You want to use the SADMIN Libraries, you need to add this section at the top of your script
+#   Please refer to the file $sadm_base_dir/lib/sadm_lib_std.txt for a description of each
+#   variables and functions available to you when using the SADMIN functions Library
 # --------------------------------------------------------------------------------------------------
-#                                   Program Variables Definitions
+# Global variables used by the SADMIN Libraries - Some influence the behavior of function in Library
+# These variables need to be defined prior to load the SADMIN function Libraries
 # --------------------------------------------------------------------------------------------------
-PN=${0##*/}                                         ; export PN             # Program name
-VER='2.2'                                           ; export VER            # Program version
-DASH=`printf %60s |tr " " "="`                      ; export DASH           # 100 dashes line
-INST=`echo "$PN" | awk -F\. '{ print $1 }'`         ; export INST           # Get script name
-HOSTNAME=`hostname -s`                              ; export HOSTNAME       # Current Host name
-OSNAME=`uname -s|tr '[:lower:]' '[:upper:]'`        ; export OSNAME         # Get OS Name AIX/LINUX
-CUR_DATE=`date +"%Y_%m_%d"`                         ; export CUR_DATE       # Current Date
-CUR_TIME=`date +"%H_%M_%S"`                         ; export CUR_TIME       # Current Time
-BASE_DIR="/sadmin"                                  ; export BASE_DIR       # Script Root Base Dir
-SYS_DIR="$BASE_DIR/sys"                             ; export BIN_DIR        # Startup/Shutdown Dir
-TMP_DIR="$BASE_DIR/tmp"                             ; export TMP_DIR        # Script Temp Dir
-LOG_DIR="$SYS_DIR"	                                ; export LOG_DIR	    # Script log directory
-LOG_FILE="${LOG_DIR}/${INST}.log"                   ; export LOG_FILE       # Script Log file
+SADM_PN=${0##*/}                           ; export SADM_PN             # Script name
+SADM_HOSTNAME=`hostname -s`                ; export SADM_HOSTNAME       # Current Host name
+SADM_VER='2.3'                             ; export SADM_VER            # Script Version
+SADM_INST=`echo "$SADM_PN" |cut -d'.' -f1` ; export SADM_INST           # Script name without ext.
+SADM_TPID="$$"                             ; export SADM_TPID           # Script PID
+SADM_EXIT_CODE=0                           ; export SADM_EXIT_CODE      # Script Exit Return Code
+SADM_BASE_DIR=${SADMIN:="/sadmin"}         ; export SADM_BASE_DIR       # SADMIN Root Base Dir.
+SADM_LOG_TYPE="B"                          ; export SADM_LOG_TYPE       # 4Logger S=Scr L=Log B=Both
+SADM_LOG_APPEND="Y"                        ; export SADM_LOG_APPEND     # Append to Existing Log ?
+SADM_MULTIPLE_EXEC="N"                     ; export SADM_MULTIPLE_EXEC  # Run many copy at same time
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadm_lib_std.sh     
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_server.sh ] && . ${SADM_BASE_DIR}/lib/sadm_lib_server.sh  
+
+# These variables are defined in sadmin.cfg file - You can also change them on a per script basis 
+SADM_SSH_CMD="${SADM_SSH} -qnp ${SADM_SSH_PORT}" ; export SADM_SSH_CMD  # SSH Command to Access Farm
+SADM_MAIL_TYPE=1                           ; export SADM_MAIL_TYPE      # 0=No 1=Err 2=Succes 3=All
+#SADM_MAX_LOGLINE=5000                       ; export SADM_MAX_LOGLINE   # Max Nb. Lines in LOG )
+#SADM_MAX_RCLINE=100                         ; export SADM_MAX_RCLINE    # Max Nb. Lines in RCH file
+#SADM_MAIL_ADDR="your_email@domain.com"      ; export ADM_MAIL_ADDR      # Email Address of owner
+#===================================================================================================
+#
+
+
 
 # --------------------------------------------------------------------------------------------------
-#                         Write infornation into the log
+#                                   This Script environment variables
 # --------------------------------------------------------------------------------------------------
-write_log()
+DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDebug Higher=+Verbose
+SADM_MAIL_TYPE=3                            ; export SADM_MAIL_TYPE     # 0=No 1=Err 2=Succes 3=All
+
+
+
+
+# --------------------------------------------------------------------------------------------------
+#                                S c r i p t    M a i n     P r o c e s s 
+# --------------------------------------------------------------------------------------------------
+main_process()
 {
-  echo -e $(date "+%C%y.%m.%d %H:%M:%S") - "$@" >> $LOG_FILE
+    sadm_writelog "*** Running SADM System Shutdown Script on $(sadm_get_fqdn)  ***"    
+    return 0                                                            # Return Default return code
 }
 
 
@@ -43,17 +70,14 @@ write_log()
 # 	                          	S T A R T   O F   M A I N    P R O G R A M
 # --------------------------------------------------------------------------------------------------
 #
-    write_log " " ;  write_log " " ;  write_log " " ;  write_log " " ;  write_log " " ;
-    write_log "${DASH}"
-    write_log "Starting the script $PN on ${HOSTNAME}"
-    write_log "Version: ${VER}"
-
-
-    # Put Server shutdown commands below
-
- 
-    # Indicate the shutdown script is finished
-    write_log " "
-    write_log "End of $PN"
-    write_log "sadmin.service startup is finished !"
-    write_log "${DASH}"
+    sadm_start                                                          # Init Env Dir & RC/Log File
+    if ! $(sadm_is_root)                                                # Is it root running script?
+        then sadm_writelog "Script can only be run by the 'root' user"  # Advise User Message
+             sadm_writelog "Process aborted"                            # Abort advise message
+             sadm_stop 1                                                # Close and Trim Log
+             exit 1                                                     # Exit To O/S
+    fi
+    main_process                                                        # Main Process
+    SADM_EXIT_CODE=$?                                                   # Save Process Exit Code
+    sadm_stop $SADM_EXIT_CODE                                           # Upd. RCH File & Trim Log 
+    exit $SADM_EXIT_CODE                                                # Exit With Global Err (0/1)
