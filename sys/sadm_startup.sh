54ae91ee61f955b1a6dff5d1d3bdfb56aa5d9a58
Jacques Duplessis <duplessis.jacques@gmail.com>
2017-06-24 20:15:29 -0400

    V2.5 Add remove of sysmon.lock file upon reboot

--- sys/sadm_startup.sh
+++ sys/sadm_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # --------------------------------------------------------------------------------------------------
 #   Author:     Jacques Duplessis
-#   Title:      sadm_startup.sh 
+#   Title:      sadm_startup.sh
 #   Date:       25 October 2015
 #   Synopsis:   This script is run when the system is started (via sadmin.service).
 #               Called by /etc/systemd/system/sadmin.service
@@ -14,6 +14,8 @@
 #               Restructure to use the SADM Library and Send email on execution.
 # Version 2.4 - June 2017
 #               Added removal of files in (pid) /sadmin/tmp at system boot
+# Version 2.5 - June 2017
+#               Added removal of sysmon lock file (/sadmin/sysmon.lock) at system boot
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPTE LE ^C
 #set -x
@@ -29,7 +31,7 @@ trap 'sadm_stop 0; exit 0' 2                                            # INTERC
 # --------------------------------------------------------------------------------------------------
 SADM_PN=${0##*/}                           ; export SADM_PN             # Script name
 SADM_HOSTNAME=`hostname -s`                ; export SADM_HOSTNAME       # Current Host name
-SADM_VER='2.4'                             ; export SADM_VER            # Script Version
+SADM_VER='2.5'                             ; export SADM_VER            # Script Version
 SADM_INST=`echo "$SADM_PN" |cut -d'.' -f1` ; export SADM_INST           # Script name without ext.
 SADM_TPID="$$"                             ; export SADM_TPID           # Script PID
 SADM_EXIT_CODE=0                           ; export SADM_EXIT_CODE      # Script Exit Return Code
@@ -37,10 +39,10 @@ SADM_BASE_DIR=${SADMIN:="/sadmin"}         ; export SADM_BASE_DIR       # SADMIN
 SADM_LOG_TYPE="B"                          ; export SADM_LOG_TYPE       # 4Logger S=Scr L=Log B=Both
 SADM_LOG_APPEND="Y"                        ; export SADM_LOG_APPEND     # Append to Existing Log ?
 SADM_MULTIPLE_EXEC="N"                     ; export SADM_MULTIPLE_EXEC  # Run many copy at same time
-[ -f ${SADM_BASE_DIR}/lib/sadm_lib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadm_lib_std.sh     
-[ -f ${SADM_BASE_DIR}/lib/sadm_lib_server.sh ] && . ${SADM_BASE_DIR}/lib/sadm_lib_server.sh  
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadm_lib_std.sh
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_server.sh ] && . ${SADM_BASE_DIR}/lib/sadm_lib_server.sh
 
-# These variables are defined in sadmin.cfg file - You can also change them on a per script basis 
+# These variables are defined in sadmin.cfg file - You can also change them on a per script basis
 SADM_SSH_CMD="${SADM_SSH} -qnp ${SADM_SSH_PORT}" ; export SADM_SSH_CMD  # SSH Command to Access Farm
 SADM_MAIL_TYPE=1                           ; export SADM_MAIL_TYPE      # 0=No 1=Err 2=Succes 3=All
 #SADM_MAX_LOGLINE=5000                       ; export SADM_MAX_LOGLINE   # Max Nb. Lines in LOG )
@@ -61,14 +63,19 @@ SADM_MAIL_TYPE=3                            ; export SADM_MAIL_TYPE     # 0=No 1
 
 
 # --------------------------------------------------------------------------------------------------
-#                                S c r i p t    M a i n     P r o c e s s 
+#                                S c r i p t    M a i n     P r o c e s s
 # --------------------------------------------------------------------------------------------------
 main_process()
 {
-    sadm_writelog "*** Running SADM System Startup Script on $(sadm_get_fqdn)  ***"    
+    sadm_writelog "*** Running SADM System Startup Script on $(sadm_get_fqdn)  ***"
 
     sadm_writelog "Removing old files in ${SADM_TMP_DIR}"
     rm -f ${SADM_TMP_DIR}/* >> $SADM_LOG 2>&1
+
+    sadm_writelog "Removing SADM System Monitor Lock File ${SADM_BASE_DIR/sysmon.lock}"
+    rm -f ${SADM_BASE_DIR}/sysmon.lock >> $SADM_LOG 2>&1
+
+    sadm_writelog " "
 
     sadm_writelog " "
     return 0                                                            # Return Default return code
@@ -87,5 +94,5 @@ main_process()
     fi
     main_process                                                        # Main Process
     SADM_EXIT_CODE=$?                                                   # Save Process Exit Code
-    sadm_stop $SADM_EXIT_CODE                                           # Upd. RCH File & Trim Log 
+    sadm_stop $SADM_EXIT_CODE                                           # Upd. RCH File & Trim Log
     exit $SADM_EXIT_CODE                                                # Exit With Global Err (0/1)
