963fc7fd3987595eef69d450d43fe3a7e005ca6f
Jacques Duplessis - MacBook <duplessis.jacques@gmail.com>
2017-07-29 15:17:24 -0400

    2 spaces lines at the end

--- bin/sadm_create_cfg2html.sh
+++ bin/sadm_create_cfg2html.sh
@@ -14,7 +14,7 @@
 # 2.5 Added support for LinuxMint (April 2017)
 # 2.7 on Mac OS
 #===================================================================================================
-# 
+#
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPTE LE ^C
 #set -x
@@ -37,10 +37,10 @@ SADM_BASE_DIR=${SADMIN:="/sadmin"}         ; export SADM_BASE_DIR       # SADMIN
 SADM_LOG_TYPE="B"                          ; export SADM_LOG_TYPE       # 4Logger S=Scr L=Log B=Both
 SADM_LOG_APPEND="N"                        ; export SADM_LOG_APPEND     # Append to Existing Log ?
 SADM_MULTIPLE_EXEC="N"                     ; export SADM_MULTIPLE_EXEC  # Run many copy at same time
-[ -f ${SADM_BASE_DIR}/lib/sadm_lib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadm_lib_std.sh     
-[ -f ${SADM_BASE_DIR}/lib/sadm_lib_server.sh ] && . ${SADM_BASE_DIR}/lib/sadm_lib_server.sh  
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadm_lib_std.sh
+[ -f ${SADM_BASE_DIR}/lib/sadm_lib_server.sh ] && . ${SADM_BASE_DIR}/lib/sadm_lib_server.sh
 
-# These variables are defined in sadmin.cfg file - You can also change them on a per script basis 
+# These variables are defined in sadmin.cfg file - You can also change them on a per script basis
 SADM_SSH_CMD="${SADM_SSH} -qnp ${SADM_SSH_PORT}" ; export SADM_SSH_CMD  # SSH Command to Access Farm
 SADM_MAIL_TYPE=1                           ; export SADM_MAIL_TYPE      # 0=No 1=Err 2=Succes 3=All
 #SADM_MAX_LOGLINE=5000                       ; export SADM_MAX_LOGLINE   # Max Nb. Lines in LOG )
@@ -70,7 +70,7 @@ DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDe
              sadm_stop 1                                                # Close and Trim Log
              exit 1                                                     # Exit To O/S
     fi
-    
+
     # Make sure that cfg2html is accessible on the server
     # If package not installed then used the SADMIN version located in $SADMIN_BASE_DIR/pkg
     # ----------------------------------------------------------------------------------------------
@@ -80,42 +80,42 @@ DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDe
                then CFG2HTML="$SADM_PKG_DIR/cfg2html/cfg2html"          # Use then one in /sadmin
                else sadm_writelog "Command 'cfg2html' was not found"    # Not Found inform user
                     sadm_writelog "We will install it now"              # Not Found inform user
-                    if [ "$(sadm_get_osname)" = "REDHAT" ] || [ "$(sadm_get_osname)" = "CENTOS" ] || 
-                       [ "$(sadm_get_osname)" = "FEDORA" ]                    
+                    if [ "$(sadm_get_osname)" = "REDHAT" ] || [ "$(sadm_get_osname)" = "CENTOS" ] ||
+                       [ "$(sadm_get_osname)" = "FEDORA" ]
                        then rpm -Uvh ${SADM_PKG_DIR}/cfg2html/cfg2html.rpm
                     fi
                     if [ "$(sadm_get_osname)" = "UBUNTU" ]   ||
-                       [ "$(sadm_get_osname)" = "DEBIAN" ]   || 
+                       [ "$(sadm_get_osname)" = "DEBIAN" ]   ||
                        [ "$(sadm_get_osname)" = "RASPBIAN" ] ||
-                       [ "$(sadm_get_osname)" = "LINUXMINT" ]                  
+                       [ "$(sadm_get_osname)" = "LINUXMINT" ]
                        then dpkg --install --force-confold ${SADM_PKG_DIR}/cfg2html/cfg2html.deb
                     fi
                     CFG2HTML=`which cfg2html >/dev/null 2>&1`           # Try Again to Locate cfg2html
                     if [ $? -ne 0 ]                                     # if Still not found
-                        then sadm_writelog "Still the command 'cfg2html' can't be found" 
+                        then sadm_writelog "Still the command 'cfg2html' can't be found"
                              sadm_writelog "Install it and re-run this script" # Not Found inform user
-                             sadm_writelog "For Ubuntu/Debian/Raspbian : " 
+                             sadm_writelog "For Ubuntu/Debian/Raspbian : "
                              sadm_writelog " - sudo dpkg --install ${SADM_PKG_DIR}/cfg2html/cfg2html.deb"
                              sadm_writelog "For RedHat/CentOS/Fedora   : "
                              sadm_writelog " - sudo rpm -Uvh ${SADM_PKG_DIR}/cfg2html/cfg2html.rpm"
-                             sadm_stop 1                                # Upd. RC & Trim Log 
-                             exit 1                                     # Exit With Error     
-                        else sadm_writelog "Package cfg2html was installed successfully" 
+                             sadm_stop 1                                # Upd. RC & Trim Log
+                             exit 1                                     # Exit With Error
+                        else sadm_writelog "Package cfg2html was installed successfully"
                      fi
                      CFG2HTML=`which cfg2html`                          # Save cfg2html path
             fi
        else CFG2HTML=`which cfg2html`                                   # Save cfg2html path
     fi
-    export CFG2HTML                                                     
-    
+    export CFG2HTML
+
     # Run CFG2HTML
     CFG2VER=`$CFG2HTML -v | tr -d '\n'`
-    sadm_writelog "$CFG2VER" ;  sadm_writelog "" 
+    sadm_writelog "$CFG2VER" ;  sadm_writelog ""
     sadm_writelog "Running : $CFG2HTML -H -o $SADM_DR_DIR"
     $CFG2HTML -H -o $SADM_DR_DIR >>$SADM_LOG 2>&1
     SADM_EXIT_CODE=$?
     sadm_writelog "Return code of the command is $SADM_EXIT_CODE"
-    
+
     # Uniformize name of cfg2html so that the domain name is not include in the name
     if [ `hostname` != `hostname -s` ]
         then mv ${SADM_DR_DIR}/$(hostname).err  ${SADM_DR_DIR}/`hostname -s`.err
@@ -125,13 +125,11 @@ DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDe
     fi
 
     # Aix version of cfg2html leave unnecessary file every time it run - Here we delete those
-    if [ "$(sadm_get_osname)" = "AIX" ]  
+    if [ "$(sadm_get_osname)" = "AIX" ]
         then rm -f ${SADM_DR_DIR}/`hostname -s`_?.txt > /dev/null 2>&1
     fi
-       
+
 
     # Go Write Log Footer - Send email if needed - Trim the Log - Update the Recode History File
-    sadm_stop $SADM_EXIT_CODE                                             # Upd. RCH File & Trim Log 
+    sadm_stop $SADM_EXIT_CODE                                             # Upd. RCH File & Trim Log
     exit $SADM_EXIT_CODE                                                  # Exit With Global Error (0/1)
-
-
