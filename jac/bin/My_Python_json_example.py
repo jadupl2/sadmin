cf1b78db641a32dbb6821d6e74ac8095880741fa
Jacques Duplessis - MacBook <duplessis.jacques@gmail.com>
2017-08-03 10:17:38 -0400

    V1.1 Initial Version - Read/Write json file

--- jac/bin/My_Python_json_example.py
+++ jac/bin/My_Python_json_example.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # 
+# Test Program to read and write json file
+#
+# 2017_08_03 JDuplessis
+#   V1.1 Initial Version
+
 import  json
 
 data = {
@@ -18,10 +23,8 @@ with open("data.json", 'r') as f:
      data = json.load(f)
 
 print data
-
 json_str = json.dumps(data)
 
 # Here is how you turn a JSON-encoded string back into a Python data structure:
 data = json.loads(json_str)
-
 print data
