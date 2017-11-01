81be067fa0541796e59d737cca0fbaeaaa2bc86f
Jacques Duplessis <duplessis.jacques@gmail.com>
2017-10-31 22:26:19 -0400

    Initial python notes

--- jac/bin/my_notes_dict.py
+++ jac/bin/my_notes_dict.py
@@ -8,60 +8,71 @@ print ('-'*80) ; print ("DICTIONNARY EXAMPLES") ; print ('-'*80)
 s
 
 
-print "..\n..",'-'*80, "USING Dictionnary"
-print "table = {'Bob': 26,'Jack': 2,'Gille': 9}"
+print ("..\n..",'-'*80, "USING Dictionnary")
+print ("table = {'Bob': 26,'Jack': 2,'Gille': 9}")
 table = {'Bob': 26,'Jack': 2,'Gille': 9}
-print "table = ", table, "YOU DO NOT CONTROL ORDER IN DICTIONNARY"
-print "table['Jack'] = " , table['Jack']
-print "len(table) = ", len(table)
-print "table.has_key('Jack') = ",table.has_key('Jack')
-print "table.has_key('coco') = ",table.has_key('coco')
-print "'Jack' in table = ", 'Jack' in table
-print "'coco' in table = ", 'coco' in table
-print "table.keys() = ",table.keys()
-print "table.values() = ",table.values()
-print "table.items() = ",table.items()
+print ("table = ", table, "YOU DO NOT CONTROL ORDER IN DICTIONNARY")
+print ("table['Jack'] = " , table['Jack'])
+print ("len(table) = ", len(table))
 
-print "\nDeleting an Entry"
-print "table = ", table
-print "del table['Jack']" 
+# has_key was removed in Python 3
+#print ("table.has_key('Jack') = ",table.has_key('Jack'))
+#print ("table.has_key('coco') = ",table.has_key('coco'))
+
+print ("if 'Jack' in table : print ('Yes')") 
+if 'Jack' in table : print ('Yes')
+print ("if 'coco' not in table : print ('Yes')")
+if 'coco' not in table : print ('Yes')
+
+print ("'Jack' in table = ", 'Jack' in table)
+print ("'coco' in table = ", 'coco' in table)
+
+print ("table.keys() = ",table.keys())
+print ("table.values() = ",table.values())
+print ("table.items() = ",table.items())
+
+print ("\nDeleting an Entry")
+print ("table = ", table)
+print ("del table['Jack']" )
 del table['Jack']
-print "table = ", table
+print ("table = ", table)
 
-print "\nAdding an Entry"
-print "table = ", table
+print ("\nAdding an Entry")
+print ("table = ", table)
 table['John'] = 33
-print "table['John'] = 33"
-print "table = ", table
+print ("table['John'] = 33")
+print ("table = ", table)
 
-print "\nChanging an entry"
-print "table = ", table
-print "table['John'] = 44"
+print ("\nChanging an entry")
+print ("table = ", table)
+print ("table['John'] = 44")
 table['John'] = 44
-print "table = ", table
+print ("table = ", table)
 
-print "\nOthers Dictionnary methods"
-print "table = ", table
-print "table.get('coco') =" , table.get('coco')
-print "table.get('coco',50) =" , table.get('coco',50)
-print "table.get('John') =" , table.get('John')
+print ("\nOthers Dictionnary methods")
+print ("table = ", table)
+print ("table.get('coco') =" , table.get('coco'))
+print ("table.get('coco',50) =" , table.get('coco',50))
+print ("table.get('John') =" , table.get('John'))
 
-print "table = ", table
+print ("table = ", table)
 wline = {'Roger':99, 'Albert':88} 
-print "wline = ", wline 
+print ("wline = ", wline)
 wline = {'Roger':99, 'Albert':88} 
-print "table.update(wline)"
+print ("table.update(wline)")
 table.update(wline)
-print "table = ", table
+print ("table = ", table)
 wline = {'Roger':100, 'John':188} 
-print "wline = ", wline
-print "table.update(wline)"
+print ("wline = ", wline)
+print ("table.update(wline)")
 table.update(wline)
-print "table = ", table
+print ("table = ", table)
 
-print "\nLooping thru Dictionnary with for loop"
-print "table = ", table
+print ("\nLooping thru Dictionnary with for loop")
+print ("table = ", table)
 for wline in table.keys():
-    print wline, '\t', table[wline]
+    print (wline, '\t', table[wline]
+
+    
 
 
---
+++ jac/bin/my_notes_numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Python Number Related Examples - Oct 2017 - J.D.
+#---------------------------------------------------------------------------------------------------
+
+
+print ("\n",'-'*80, "Working with Number" )
+print ("x = 1" )
+x = 1
+
+print ("x = x + 1")
+x = x + 1
+print ("x =", x)
+
+print ("x += 1")
+x += 1
+print ("x =", x)
