7d9743c77ddaa9f55c439e7bb17ec36229534969
Jacques Duplessis <duplessis.jacques@gmail.com>
2017-12-31 11:15:42 -0500

    V2.1 Include Function to update User Crontab (permit tun run O/s Update Schedule)

--- www/lib/sadmLib.php
+++ www/lib/sadmLib.php
@@ -25,6 +25,8 @@
 #       V1.0 Initial Version
 #   2017_11_15 - Jacques Duplessis
 #       V2.0 Restructure and modify to used to new web interface and MySQL Database.
+#   2017_12_31 - Jacques Duplessis
+#       V2.1 Include Function to update User Crontab (permit tun run O/s Update Schedule)
 #
 # ==================================================================================================
 #
@@ -65,7 +67,6 @@ function display_std_heading($BACK_URL,$LTITLE,$CTITLE,$RTITLE,$WVER,
     }
     echo "\n<div style='clear: both;'> </div>";                         # Clear Move Down Now
     echo "\n<hr/>";                                                     # Print Horizontal Line
-    #echo "\n<br>\n";                                                    # Print Horizontal Line
 }
 
 
@@ -91,7 +92,7 @@ function std_page_footer($wcon) {
 # ==================================================================================================
 #                      Update the crontab based on the $paction parameter
 #
-#   pscript  The name of the script to execute (With the path) 
+#   pscript  The name of the script to execute (With NO path - Need to be in $SADMIN/bin)
 #   pname    The hostname of the server to include/modify/delete in crontab
 #   paction  [C] for create entry  [U] for Updating the crontab   [D] Delete crontab entry
 #   pmonth   13 Characters (either a Y or a N) each representing a month (YNNNNNNNNNNNN)
@@ -121,12 +122,26 @@ function std_page_footer($wcon) {
 function update_crontab ($pscript,$paction = "U",$pmonth = "YNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
                         $pdom = "YNNNNNNNNNNNN",$pdow="NNNNNNNY",$phour=01,$pmin=00) 
 {
-    if ($DEBUG) { echo "<BR>I'm in update crontab" ; } 
+    # To Display Parameters received - Used for Debugging Purpose ----------------------------------
+    $DEBUG = False;
+    if ($DEBUG) { 
+        echo "<BR>I'm in update crontab" ; 
+        $wmsg1 = "\npscript = " . $pscript ;                            # Script to execute
+        $wmsg2 = "\npaction = " . $paction ;                            # U=Update C=Create D=Delete
+        $wmsg3 = "\npmonth  = " . $pmonth ;                             # Month String YNYNYNYNYNY..
+        $wmsg4 = "\npdom    = " . $pdom;                                # Day of MOnth String YNYN..
+        $wmsg5 = "\npdow    = " . $pdow;                                # Day of Week String YNYN...
+        $wmsg6 = "\nphour   = " . $phour;                               # Hour to run script
+        $wmsg7 = "\npmin    = " . $pmin;                                # Min. to run Script
+        $wmsg  = $wmsg1.$wmsg2.$wmsg3.$wmsg4.$wmsg5.$wmsg6.$wmsg7 ;     # Combine information
+        sadm_alert ($wmsg) ;                                            # Display Alert with Info
+        #return ;
+    } 
     
-    # Begin constructing our crontab line ($cline) based on parameters received
+    # Begin constructing our crontab line ($cline) - Based on Hour and Min. Received ---------------
     $cline = sprintf ("%02d %02d ",$pmin,$phour);                       # Hour & Min. of Execution
 
-    # Construct Date of the month (1-31) to run and add it to crontab line ($cline)
+    # Construct Date of the month (1-31) to run and add it to crontab line ($cline) ----------------
     if ($pdom == "YNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN") {                  # If it's to run every Date
         $cline = $cline . "* ";                                         # Then use a Star
     }else{                                                              # If not to run every Date 
@@ -143,7 +158,7 @@ function update_crontab ($pscript,$paction = "U",$pmonth = "YNNNNNNNNNNNNNNNNNNN
         $cline = $cline . $cdom . " " ;                                 # Add DOM in Crontab Line
     }
     
-    # Construct the month(s) (1-12) to run and add it to crontab line ($cline)
+    # Construct the month(s) (1-12) to run and add it to crontab line ($cline) ---------------------
     if ($pmonth == "YNNNNNNNNNNNN") {                                   # If it's to run every Month
         $cline = $cline . "* ";                                         # Then use a Star
     }else{                                                              # If not to run every Months
@@ -160,7 +175,7 @@ function update_crontab ($pscript,$paction = "U",$pmonth = "YNNNNNNNNNNNNNNNNNNN
         $cline = $cline . $cmonth . " " ;                               # Add Month in Crontab Line
     }
     
-    # Construct the day of the week (0-6) to run and add it to crontab line ($cline)
+    # Construct the day of the week (0-6) to run and add it to crontab line ($cline) ---------------
     if ($pdow == "YNNNNNNN") {                                          # If it's to run every Day
         $cline = $cline . "* ";                                         # Then use a Star
     }else{                                                              # If not to run every Day
@@ -168,43 +183,46 @@ function update_crontab ($pscript,$paction = "U",$pmonth = "YNNNNNNNNNNNNNNNNNNN
         for ($i = 1; $i < 8; $i = $i + 1) {                             # Check for Y or N in Week
             if (substr($pdow,$i,1) == "Y") {                            # If Yes to run that Day
                 if (strlen($cdow) == 0) {                               # And first add day of week
-                    $cdow = sprintf("%02d",$i);                         # Add Day number 
+                    $cdow = sprintf("%02d",$i-1);                       # Add Day number 
                 }else{                                                  # If not 1st Day in list
-                    $cdow = $cdow . "," . sprintf("%02d",$i);           # More Day add , before Day
+                    $cdow = $cdow . "," . sprintf("%02d",$i-1);         # More Day add , before Day
                 }
             }    
         }
         $cline = $cline . $cdow . " " ;                                 # Add Month in Crontab Line
     }
     
-    # Add User, script name and script parameter to crontab line
-    $cline = $cline . "root " . $pscript . " >/dev/null 2>&1\n";        # Add user & cmd on $cline
+    # Add User, script name and script parameter to crontab line -----------------------------------
+    # SCRIPT WILL RUN ONLY IF LOCATED IN $SADMIN/BIN
+    $cline = $cline.SADM_USER." sudo ".SADM_BIN_DIR."/".$pscript." >/dev/null 2>&1\n";   
     if ($DEBUG) { echo "\n<br>Final crontab line : " . $cline ; }       # Debug Show crontab line
    
-    # Opening what will become the new crontab file
-    $newtab = fopen(SADM_WWW_TMP_FILE1,"w");                            # Create new Crontab File
+    # Opening what will become the new crontab file ------------------------------------------------
+    $newtab = fopen(SADM_WWW_TMP_FILE3,"w");                            # Create new Crontab File
     if (! $newtab) {                                                    # If Create didn't work
-        sadm_fatal_error ("Can't create file " . SADM_WWW_TMP_FILE1) ;  # Show Err & Back Prev. Page
+        sadm_fatal_error ("Can't create file " . SADM_WWW_TMP_FILE3) ;  # Show Err & Back Prev. Page
     } 
-    # Write Crontab File Header
+
+    # Write Crontab File Header --------------------------------------------------------------------
     $wline = "# Please don't edit manually, SADMIN generated file ". date("Y-m-d H:i:s") ."\n"; 
     fwrite($newtab,$wline);                                             # Write SADM Cron Header
     fwrite($newtab,"# \n");                                             # Write Comment Line
+    fwrite($newtab,"SADMIN=".SADM_BASE_DIR."\n");                       # Write SADMIN Dir. Location
+    fwrite($newtab,"# \n");                                             # Write Comment Line
 
-    # Open existing crontab File - If don't exist create empty one
+    # Open existing crontab File - If don't exist create empty one ---------------------------------
     if (!file_exists(SADM_CRON_FILE)) {                                 # SADM crontab doesn't exist
         touch(SADM_CRON_FILE);                                          # Create empty crontab file
         chmod(SADM_CRON_FILE,0640);                                     # Set Permission on crontab
-        #chown(SADM_CRON_FILE,'root');                                   # Set Crontab Owner
-        #chgrp(SADM_CRON_FILE,'root');                                   # Set Crontab Group Owner
     }
 
-    # Load actual crontab in array and process each line in array
+    # Load actual crontab in array and process each line in array ----------------------------------
     $alines = file(SADM_CRON_FILE);                                     # Load Crontab in Array
     $UPD_DONE = False;                                                  # AutoUpdate was Off now ON?
     foreach ($alines as $line_num => $line) {                           # Process each line in Array
         if ($DEBUG) { echo "\n<br>Before Processing Ligne #{$line_num} : " . $line ; }        
         if (strpos(trim($line), '#') === 0) continue;                   # Next line if comment
+        if (strpos(trim($line), 'SADMIN=') === 0) continue;             # Next line if SADMIN Env.
         $line = trim($line);                                            # Trim Crontab Line
         $wpos = strpos($line,$pscript);                                 # Get Pos. of script on line
         if ($wpos == false) {                                           # If Script is not on line
@@ -216,10 +234,10 @@ function update_crontab ($pscript,$paction = "U",$pmonth = "YNNNNNNNNNNNNNNNNNNN
     if ($paction == 'C') { fwrite($newtab,$cline); }                    # Add new line to crontab 
     if ($paction == "U") { fwrite($newtab,$cline); }                    # Add Update line to crontab 
     fclose($newtab);                                                    # Close sadm new crontab
-    if (! copy(SADM_WWW_TMP_FILE1,SADM_CRON_FILE)) {                    # Copy new over existing 
-        sadm_fatal_error ("Error copying " . SADM_WWW_TMP_FILE1 . " to " . SADM_CRON_FILE . " ");
+    if (! copy(SADM_WWW_TMP_FILE3,SADM_CRON_FILE)) {                    # Copy new over existing 
+        sadm_fatal_error ("Error copying " . SADM_WWW_TMP_FILE3 . " to " . SADM_CRON_FILE . " ");
     }
-    unlink(SADM_WWW_TMP_FILE1);                                         # Delete Crontab tmp file
+    unlink(SADM_WWW_TMP_FILE3);                                         # Delete Crontab tmp file
 }
 
 
