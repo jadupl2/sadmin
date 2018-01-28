da560383df4ee892a7411e5d47a13b5a884e30e8
Jacques Duplessis - Dell Linux <duplessis.jacques@gmail.com>
2018-01-28 11:50:22 -0500

    sadm_startup.sh - V3.0a Start 'nmon' as standard startup procedure

--- sys/sadm_startup.sh
+++ sys/sadm_startup.sh
@@ -41,7 +41,7 @@ trap 'sadm_stop 0; exit 0' 2                                            # INTERC
 # --------------------------------------------------------------------------------------------------
 SADM_PN=${0##*/}                           ; export SADM_PN             # Script name
 SADM_HOSTNAME=`hostname -s`                ; export SADM_HOSTNAME       # Current Host name
-SADM_VER='2.9'                             ; export SADM_VER            # Script Version
+SADM_VER='3.0'                             ; export SADM_VER            # Script Version
 SADM_INST=`echo "$SADM_PN" |cut -d'.' -f1` ; export SADM_INST           # Script name without ext.
 SADM_TPID="$$"                             ; export SADM_TPID           # Script PID
 SADM_EXIT_CODE=0                           ; export SADM_EXIT_CODE      # Script Exit Return Code
