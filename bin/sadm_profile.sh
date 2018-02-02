bec61019d99a1abb4657865aa13ea9c66952eb05
Jacques Duplessis - Dell Linux <duplessis.jacques@gmail.com>
2018-02-02 16:51:33 -0500

    Set SADMIN Env. - /etc/profile.d/sadmin

---
+++ bin/sadm_profile.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env sh
+# --------------------------------------------------------------------------------------------------
+#   Author      :   Jacques Duplessis
+#   Title       :   sadm_profile.sh
+#   Synopsis    :   This Script set the necessary environment variable for SADMIN Tools.
+#   Version     :   1.0
+#   Date        :   5 January 2016
+#   Requires    :   sh and SADMIN Library
+#   Description :
+#
+#   This code was originally written by Jacques Duplessis <duplessis.jacques@gmail.com>,
+#   Copyright (C) 2016-2018 Jacques Duplessis <jacques.duplessis@sadmin.ca> - http://www.sadmin.ca
+#
+#   The SADMIN Tool is free software; you can redistribute it and/or modify it under the terms
+#   of the GNU General Public License as published by the Free Software Foundation; either
+#   version 2 of the License, or (at your option) any later version.
+
+#   SADMIN Tools are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program.
+#   If not, see <http://www.gnu.org/licenses/>.
+# 
+# --------------------------------------------------------------------------------------------------
+# CHANGELOG
+# 2018_01_31 JDuplessis V2.0 - Minor Changes
+# --------------------------------------------------------------------------------------------------
+#
+# Set the SADMIN Base Directory
+export SADMIN="/sadmin"                                                 # SADMIN root Directory
