ce843bc0aa6fb0c37aaa485d34e99df69b9734a4
Jacques Duplessis <jacques@tcentos7.maison.ca>
2018-04-04 11:25:59 -0400

    Test program for Maraidb

--- setup/jac/check_user.py
+++ setup/jac/check_user.py
@@ -8,6 +8,19 @@ except ImportError as e:
     print ("Import Error : %s " % e)
     sys.exit(1)
 
+# Text Colors Attributes Class
+class color:
+    PURPLE      = '\033[95m'
+    CYAN        = '\033[96m'
+    DARKCYAN    = '\033[36m'
+    BLUE        = '\033[94m'
+    GREEN       = '\033[92m'
+    YELLOW      = '\033[93m'
+    RED         = '\033[91m'
+    BOLD        = '\033[1m'
+    UNDERLINE   = '\033[4m'
+    END         = '\033[0m'
+
 def oscommand(command) :
     #print ("In sadm_oscommand function to run command : %s\n" % (command))
     p = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
@@ -20,7 +33,7 @@ def oscommand(command) :
 #   Test if user received (uname) exist in the MariaDB, root password must is needed (dbroot_pwd)                                 M A I N     P R O G R A M
 #===================================================================================================
 #
-def user_exit(uname,dbroot_pwd):
+def user_exist(uname,dbroot_pwd):
     userfound = False
     sql = "select User from mysql.user where User='%s';" % (uname) 
     cmd = "mysql -u root -p%s -e \"%s\"" % (dbroot_pwd,sql)
@@ -32,24 +45,83 @@ def user_exit(uname,dbroot_pwd):
     return (userfound)
 
 #===================================================================================================
+#   Test if sadmin database exist
+#===================================================================================================
+#
+def database_exist(dbname,dbroot_pwd):
+    dbfound = False
+    sql = 'show databases;'
+    cmd = "mysql -u root -p%s -e \"%s\"" % (dbroot_pwd,sql)
+    ccode,cstdout,cstderr = oscommand(cmd)                              # Execute MySQL Command 
+    #print ("Error code returned is %d \n%s" % (ccode,cmd))       # Show Return Code No
+    #print ("Standard out is %s" % (cstdout))                     # Print command stdout
+    #print ("Standard error is %s" % (cstderr))                   # Print command stderr
+    #print ("type is %s " % (type(cstdout)))
+    if (ccode == 0):
+        listdb = cstdout.splitlines()
+        for db in listdb:
+             if (db == dbname) : dbfound = True
+    return (dbfound)
+
+
+def askyesno(emsg,sdefault="Y"):
+    while True:
+        wmsg = emsg + " (" + color.DARKCYAN + color.BOLD + "Y/N" + color.END + ") " 
+        wmsg += "[" + sdefault + "] ? "
+        wanswer = input(wmsg)
+        #print ("Answer is %s len of %d of type %s " % (wanswer,len(wanswer),type(wanswer)))
+        if (len(wanswer) == 0) : wanswer=sdefault
+        if ((wanswer.upper() == "Y") or (wanswer.upper() == "N")):
+            break
+        else:
+            print ("Please respond by 'Y' or 'N'\n")
+    if (wanswer.upper() == "Y") : 
+        return True
+    else:
+        return False
+
+
+#==================================================================================================
 #                                  M A I N     P R O G R A M
 #===================================================================================================
 #
 def main():
+    answer=askyesno('Want to load initial Database (Will erase actual content of Database)','N')
+    if (answer): 
+        print ("Respond Yes")
+    else:
+        print ("Respond No")
+
+
     dbroot_pwd = "jacques"
 #    wcfg_rw_dbpwd = "Nimdas17"
  
     uname = "sadmin"
-    if (user_exit(uname,dbroot_pwd)):
+    if (user_exist(uname,dbroot_pwd)):
         print ("User %s exist" % (uname))
     else:
         print ("User %s don't exist" % (uname))
         
     uname = "batman"
-    if (user_exit(uname,dbroot_pwd)):
+    if (user_exist(uname,dbroot_pwd)):
         print ("User %s exist" % (uname))
     else:
         print ("User %s don't exist" % (uname))
+
+    print ("\n\n")        
+    dbname = "sadmin"
+    if not (database_exist(dbname,dbroot_pwd)):
+        print ("Database %s don't exist" % (dbname))
+    else:
+        print ("Database %s exist" % (dbname))
+        
+    dbname = "batcave"
+    if not (database_exist(dbname,dbroot_pwd)):
+        print ("Database %s don't exist" % (dbname))
+    else:
+        print ("Database %s exist" % (dbname))
+
+        
         
     # # Create 'sadmin' user and Grant permission 
     # print ("Creating 'sadmin' user ... ")
