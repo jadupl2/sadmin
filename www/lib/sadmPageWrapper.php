0191966613c57b7e57335b26026409333e26e929
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-04-15 10:08:18 -0400

    Site page header Changes

--- www/lib/sadmPageWrapper.php
+++ www/lib/sadmPageWrapper.php
@@ -3,25 +3,18 @@
     echo "\n<body>\n";
 
     echo "\n<div id='sadmWrapper'>                  <!-- Start Of sadmWrapper -->\n";
-    echo "\n<div id='sadmHeader'>                   <!-- Start Of sadmHeader  -->\n";
 
-    #echo "\n<div id='sadmLogo'><a href='/index.php'>";
-    #echo "<img width=48 height=48 src=/images/sadmin_logo.png></a>";
-    #echo "</div>";
-
-    echo "\n<div id='sadmEnteteLine1'>";
+    echo "\n<div id='sadmHeader'>                   <!-- Start Of sadmHeader  -->";
+    echo "\n<div id='sadmEnteteLine1'>              <!-- Start Of sadmEnteteLine1 -->\n";
+    echo "\n    <div id='sadmLogo'><a href='/index.php'>";
+    echo "           <img width=48 height=48 src=/images/sadmin_logo.png></a>";
+    echo "</div>";
     echo "\n    <div id='sadmEntete1L'>SADMIN Control Center</div>";
-    echo "\n    <div id='sadmEntete1R'>The Unix Servers Farm Control Environment</div>"; 
-    #echo "\n</div>\n<br>";
-    echo "\n</div>\n";
-
-    echo "\n<div id='sadmEnteteLine2'>";
-    echo "\n    <div id='sadmEntete2L'>" . SADM_CIE_NAME . "</div>";
-    echo "\n    <div id='sadmEntete2R'>Release " . SADM_VERSION . "</div>";
-    echo "\n</div>";
-
-    echo "\n    <div style='clear: both;'> </div><br>";
-
+    echo "\n    <div id='sadmEntete1R'>The Unix Servers Farm Control Environment - Release " .SADM_VERSION."</div>"; 
+    echo "\n    <div id='sadmEntete1C'> - " . SADM_CIE_NAME . "</div>";
+    echo "\n</div>                                  <!-- End Of sadmEnteteLine1 -->\n";
+    echo "\n    <div style='clear: both;'> </div>";
+    echo "\n</div>                                  <!-- End of sadmHeader -->\n\n";
 
     # ----------------------------------------------------------------------------------------------
     # HEADING MENU LINE 
@@ -38,7 +31,7 @@
     #echo "\n</div>                                  <!-- End Of sadmTopMenu -->\n\n";
 
     # ----------------------------------------------------------------------------------------------
-    echo "\n</div>                                  <!-- End of Div sadmHeader -->\n\n";
+    #echo "\n</div>                                  <!-- End of Div sadmHeader -->\n\n";
 
     # ----------------------------------------------------------------------------------------------
     echo "\n<div id='sadmPageContents'>             <!-- Start Of sadmPageContents -->\n\n";
