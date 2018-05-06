4098bee2baf38b7c3077ae1663d743e850ef5ed6
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-05-06 10:39:49 -0400

    V2.21 Change name of crontab file in etc/cron.d to sadm_server_osupdate

--- lib/sadmlib_std.sh
+++ lib/sadmlib_std.sh
@@ -49,6 +49,8 @@
 #   V2.19 Add SADM_HOST_TYPE field in sadmin.cfg
 # 2018_05_03 JDuplessis
 #   V2.20 Password for Database Standard User (sadmin and squery) now read from .dbpass file
+# 2018_05_06 JDuplessis
+#   V2.21 Change name of crontab file in etc/cron.d to sadm_server_osupdate
 #===================================================================================================
 trap 'exit 0' 2                                                         # Intercepte The ^C    
 #set -x
@@ -67,7 +69,7 @@ SADM_VAR1=""                                ; export SADM_VAR1          # Temp D
 SADM_STIME=""                               ; export SADM_STIME         # Script Start Time
 SADM_DEBUG_LEVEL=0                          ; export SADM_DEBUG_LEVEL   # 0=NoDebug Higher=+Verbose
 DELETE_PID="Y"                              ; export DELETE_PID         # Default Delete PID On Exit 
-SADM_LIB_VER="2.20"                         ; export SADM_LIB_VER       # This Library Version
+SADM_LIB_VER="2.21"                         ; export SADM_LIB_VER       # This Library Version
 #
 # SADMIN DIRECTORIES STRUCTURES DEFINITIONS
 SADM_BASE_DIR=${SADMIN:="/sadmin"}          ; export SADM_BASE_DIR      # Script Root Base Dir.
@@ -108,7 +110,7 @@ SADM_PID_FILE="${SADM_TMP_DIR}/${SADM_INST}.pid"            ; export SADM_PID_FI
 SADM_CFG_FILE="$SADM_CFG_DIR/sadmin.cfg"                    ; export SADM_CFG_FILE   # Cfg file name
 SADM_REL_FILE="$SADM_CFG_DIR/.release"                      ; export SADM_REL_FILE   # Release Ver.
 SADM_CRON_FILE="$SADM_WWW_LIB_DIR/.crontab.txt"             ; export SADM_CRON_FILE  # Work crontab
-SADM_CRONTAB="/etc/cron.d/sadmin"                           ; export SADM_CRONTAB    # Final crontab
+SADM_CRONTAB="/etc/cron.d/sadm_server_osupdate"             ; export SADM_CRONTAB    # Final crontab
 SADM_CFG_HIDDEN="$SADM_CFG_DIR/.sadmin.cfg"                 ; export SADM_CFG_HIDDEN # Cfg file name
 SADM_TMP_FILE1="${SADM_TMP_DIR}/${SADM_INST}_1.$$"          ; export SADM_TMP_FILE1  # Temp File 1 
 SADM_TMP_FILE2="${SADM_TMP_DIR}/${SADM_INST}_2.$$"          ; export SADM_TMP_FILE2  # Temp File 2
