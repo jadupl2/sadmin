5922a344ca7be9db106387fafd65e0f5aa29a8c0
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-05-27 10:13:54 -0400

    My Python QuickRef Examples Update

--- usr/bin/QuickRef_Python/Open_Read_Write_File.py
+++ usr/bin/QuickRef_Python/Open_Read_Write_File.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Python Files Related Examples - Oct 2017 - J.D.
 #---------------------------------------------------------------------------------------------------
+import os
 
 print ("\n",'-'*80, "Working with file")
 print ("myfile = open('datafile', 'w')")
@@ -10,6 +11,7 @@ myfile.write('hello world !\n')
 print ("myfile.close()")
 myfile.close()
 
+
 print ("\nmyfile = open('datafile', 'r')")
 myfile = open('datafile', 'r')
 print ("myfile.readline()")
@@ -18,3 +20,6 @@ print ("myfile.readline()")
 myfile.readline()
 print ("myfile.close()")
 myfile.close()
+
+os.remove('datafile')
+
