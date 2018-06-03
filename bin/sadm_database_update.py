ea03790a28dd7723d6ecf581a928980656692f23
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-06-03 08:56:40 -0400

    v3.0 Adapt to new SADMIN Python Library

--- bin/sadm_database_update.py
+++ bin/sadm_database_update.py
@@ -2,8 +2,7 @@
 #===================================================================================================
 #   Author:     Jacques Duplessis
 #   Title:      sadm_database_update.py
-#   Synopsis:   Read all /sadmin/www/dat/{HOSTNAME}/dr/{HOSTNAME}&  update the Database based 
-#               on the content of these files.
+#   Synopsis:   Read Hardware/Software/Performance data collected from servers & update database
 #===================================================================================================
 # Description
 #
@@ -20,24 +19,14 @@
 #   You should have received a copy of the GNU General Public License along with this program.
 #   If not, see <http://www.gnu.org/licenses/>.
 #===================================================================================================
-#  March 2017 - Jacques Duplessis
-#       V2.3 -  To eliminate duplicate field separator with Mac Address 
-#               Change field separator in HOSTNAME_sysinfo.txt file from : to = 
-#               Add Error Exception when getting Value
-#               Add time to Last_Daily_Update Column (Timestamp) in the SADM Database
-#
-# April 2017 -  Jacques Duplessis 
-#       V2.4 -  Change exception Error Message more verbose
-#
-# 2017_11_23 -  Jacques Duplessis
-#       V2.6 -  Move from PostGres to MySQL, New SADM Python Library and Performance Enhanced.
-#
-# 2017_12_07 -  Jacques Duplessis
-#       V2.7 -  Correct Problem dealing with srv_date_update on newly added server.
-# 2018_02_08 -  Jacques Duplessis
-#       V2.8 -  Bug Fix with test for physical/virtual server
-# 2018_02_21 - JDuplessis
-#       V2.9 - Adjust to new calling sadmin lib method#
+# Change Log
+# March 2017    v2.3 Change field separator in HOSTNAME_sysinfo.txt file from : to = 
+# April 2017    v2.4 Change exception Error Message more verbose
+# 2017_11_23    v2.6 Move from PostGres to MySQL, New SADM Python Library and Performance Enhanced.
+# 2017_12_07    v2.7 Correct Problem dealing with srv_date_update on newly added server.
+# 2018_02_08    v2.8 Bug Fix with test for physical/virtual server
+# 2018_02_21    v2.9 Adjust to new calling sadmin lib method#
+# 2018_06_03    v3.0 Adapt to new SADMIN Python Library
 #
 #===================================================================================================
 #
@@ -59,6 +48,49 @@ except ImportError as e:
 #===================================================================================================
 #
 wdict           = {}                                                    # Dict for Server Columns
+
+
+#===================================================================================================
+# Setup SADMIN Global Variables and Load SADMIN Python Library
+#===================================================================================================
+def setup_sadmin():
+
+    # Load SADMIN Standard Python Library
+    try :
+        SADM = os.environ.get('SADMIN')                     # Getting SADMIN Root Dir.
+        sys.path.insert(0,os.path.join(SADM,'lib'))         # Add SADMIN to sys.path
+        import sadmlib_std as sadm                          # Import SADMIN Python Libr.
+    except ImportError as e:                                # If Error importing SADMIN 
+        print ("Error Importing SADMIN Module: %s " % e)    # Advise USer of Error
+        sys.exit(1)                                         # Go Back to O/S with Error
+    
+    # Create Instance of SADMIN Tool
+    st = sadm.sadmtools()                       # Create SADMIN Tools Instance (Setup Dir.,Var,...)
+
+    # Change these values to your script needs.
+    st.ver              = "3.0"                 # Current Script Version
+    st.multiple_exec    = "N"                   # Allow running multiple copy at same time ?
+    st.log_type         = 'B'                   # Output goes to [S]creen [L]ogFile [B]oth
+    st.log_append       = True                  # Append Existing Log or Create New One
+    st.use_rch          = True                  # Generate entry in Return Code History (.rch) 
+    st.log_header       = True                  # Show/Generate Header in script log (.log)
+    st.log_footer       = True                  # Show/Generate Footer in script log (.log)
+    st.usedb            = True                  # True=Open/Use Database,False=Don't Need to Open DB 
+    st.dbsilent         = False                 # Return Error Code & False=ShowErrMsg True=NoErrMsg
+    st.exit_code        = 0                     # Script Exit Code for you to use
+
+    # Override Default define in $SADMIN/cfg/sadmin.cfg
+    #st.cfg_mail_type    = 1                    # 0=NoMail 1=OnlyOnError 2=OnlyOnSucces 3=Allways
+    #st.cfg_mail_addr    = ""                   # This Override Default Email Address in sadmin.cfg
+    #st.cfg_cie_name     = ""                   # This Override Company Name specify in sadmin.cfg
+    #st.cfg_max_logline  = 5000                 # When Script End Trim log file to 5000 Lines
+    #st.cfg_max_rchline  = 100                  # When Script End Trim rch file to 100 Lines
+    #st.ssh_cmd = "%s -qnp %s " % (st.ssh,st.cfg_ssh_port) # SSH Command to Access Server 
+
+    # Start SADMIN Tools - Initialize 
+    st.start()                                  # Create dir. if needed, Open Log, Update RCH file..
+    return(st)                                  # Return Instance Obj. To Caller
+
 
 
 
@@ -345,39 +377,28 @@ def process_servers(wconn,wcur,st):
 #
 def main():
 
-    # SADMIN TOOLS - Create SADMIN instance & setup variables specific to your program -------------
-    st = sadm.sadmtools()                       # Create SADMIN Tools Instance (Setup Dir.)
-    st.ver  = "2.9"                             # Indicate this script Version 
-    st.multiple_exec = "N"                      # Allow to run Multiple instance of this script ?
-    st.log_type = 'B'                           # Log Type  (L=Log file only  S=stdout only  B=Both)
-    st.log_append = True                        # True=Append existing log  False=start a new log
-    st.debug = 0                                # Debug level and verbosity (0-9)
-    st.cfg_mail_type = 1                        # 0=NoMail 1=OnlyOnError 2=OnlyOnSucces 3=Allways
-    st.usedb = True                             # True=Use Database  False=DB Not needed for script
-    st.dbsilent = False                         # Return Error Code & False=ShowErrMsg True=NoErrMsg
-    #st.cfg_mail_addr = ""                      # This Override Default Email Address in sadmin.cfg
-    #st.cfg_cie_name  = ""                      # This Override Company Name specify in sadmin.cfg
-    st.start()                                  # Create dir. if needed, Open Log, Update RCH file..
+    # Script can only be run by the user root (Optional Code)
+    if not os.getuid() == 0:                                            # UID of user is not root
+       print ("This script must be run by the 'root' user")             # Advise User Message / Log
+       print ("Try sudo %s" % (os.path.basename(sys.argv[0])))          # Suggest to use 'sudo'
+       print ("Process aborted")                                        # Process Aborted Msg
+       sys.exit(1)                                                      # Exit with Error Code
 
+    # Import SADMIN Module, Create SADMIN Tool Instance, Initialize Log and rch file.  
+    st = setup_sadmin()                                                 # Setup Var. & Load SADM Lib
 
-    # Test if script is run by root (Optional Code)
-    #if os.geteuid() != 0:                                               # UID of user is not zero
-    #    st.writelog("This script must be run by the 'root' user")       # Advise User Message / Log
-    #    st.writelog("Process aborted")                                  # Process Aborted Msg
-    #    st.stop(1)                                                      # Close and Trim Log/Email
-    #    sys.exit(1)                                                     # Exit with Error Code
-    
     # Test if script is running on the SADMIN Server, If not abort script (Optional code)
     if st.get_fqdn() != st.cfg_server:                                  # Only run on SADMIN
         st.writelog("This script can only be run on SADMIN server (%s)" % (st.cfg_server))
         st.writelog("Process aborted")                                  # Abort advise message
         st.stop(1)                                                      # Close and Trim Log
         sys.exit(1)                                                     # Exit To O/S
-
+        
     (conn,cur) = st.dbconnect()                                         # Connect to SADMIN Database
     st.exit_code = process_servers(conn,cur,st)                         # Process Actives Servers 
     st.dbclose()                                                        # Close the Database
     st.stop(st.exit_code)                                               # Close SADM Environment
+    sys.exit(st.exit_code)                                              # Exit To O/S
 
 # This idiom means the below code only runs when executed from command line
 if __name__ == '__main__':  main()
