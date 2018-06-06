adf146282487e8c832e2d36744d4144e323dd3ec
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-06-06 14:57:33 -0400

    v2.1 NFS Server Name, Mount Point and Nb of copy to keep are taken from sadmin.cfg

--- bin/sadm_rear_backup.sh
+++ bin/sadm_rear_backup.sh
@@ -2,11 +2,30 @@
 # --------------------------------------------------------------------------------------------------
 #   Author   :  Jacques Duplessis
 #   Title    :  sadm_rear_backup.sh
-#   Synopsis :  This script produce an bootable iso and a backup restorable after booting from iso.
+#   Synopsis :  Produce a ReaR bootable iso and a restorable backup on an NFS server
 #   Version  :  1.6
 #   Date     :  14 December 2016
 #   Requires :  sh
 #
+#   The Values for the NFS server, NFS mount point and number of backup to keep are taken from
+#   SADMIN configuration file ($SADMIN/cfg/sadmin.cfg ). 
+#   You need to change them to reflect your environment.
+#
+#       $SADM_REAR_NFS_SERVER               # ReaR NFS Server where backup will be stored
+#       $SADM_REAR_NFS_MOUNT_POINT          # ReaR Mount Point exported on the NFS Server
+#       $SADM_REAR_BACKUP_TO_KEEP           # ReaR Backup, Number of copies to keep
+#
+#   Every time this script is run the line below is updated in /etc/rear/site.conf to reflect 
+#   the value taken from $SADMIN/cfg/sadmin.cfg
+#   
+#       BACKUP_URL="nfs://${SADM_REAR_NFS_SERVER}/${SADM_REAR_NFS_MOUNT_POINT}/"
+#
+#   The ReaR crontab file is remove, every time you run this script.
+#   This is to decide when we run rear from this script.
+#   If you don't want to remove it, put this line in comment at the end of this script.
+#       if [ -r /etc/cron.d/rear ] ; then rm -f /etc/cron.d/rear >/dev/null 2>&1; fi
+#
+# --------------------------------------------------------------------------------------------------
 #   Copyright (C) 2016 Jacques Duplessis <duplessis.jacques@gmail.com>
 #
 #   The SADMIN Tool is free software; you can redistribute it and/or modify it under the terms
@@ -20,60 +39,116 @@
 #   You should have received a copy of the GNU General Public License along with this program.
 #   If not, see <http://www.gnu.org/licenses/>.
 # --------------------------------------------------------------------------------------------------
+# Change Log
 #
-#  History
-#  1.7  Dec 2016    Add Check to see if Rear Configuration file isnt't present, abort Job (Exit 1)
-#  1.8  Mar 2017    Move test if rear is installed first, if not abort process up front.
-#                   Error Message more verbose and More Customization
-#  2.0  Apr 2017    Return Code returned by Rear handle correctly
-#                   Script Messages more informative
+#  2016_12_14   v1.7 Add Check to see if Rear Configuration file isnt't present, abort Job (Exit 1)
+#  2017_03_09   v1.8 Move test if rear is installed first, if not abort process up front.
+#                    Error Message more verbose and More Customization
+#  2017_04_20   v2.0 Return Code returned by Rear handle correctly, Script Messages more informative
+#  2017_06_06   v2.1 NFS Server Name, Mount Point and Nb of copy to keep are taken from sadmin.cfg
+#
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPTE LE ^C
 #set -x
 
 
-#===================================================================================================
-# If You want to use the SADMIN Libraries, you need to add this section at the top of your script
-#   Please refer to the file $sadm_base_dir/lib/sadm_lib_std.txt for a description of each
-#   variables and functions available to you when using the SADMIN functions Library
-# --------------------------------------------------------------------------------------------------
-# Global variables used by the SADMIN Libraries - Some influence the behavior of function in Library
-# These variables need to be defined prior to load the SADMIN function Libraries
-# --------------------------------------------------------------------------------------------------
-SADM_PN=${0##*/}                           ; export SADM_PN             # Script name
-SADM_HOSTNAME=`hostname -s`                ; export SADM_HOSTNAME       # Current Host name
-SADM_VER='2.0'                             ; export SADM_VER            # Script Version
-SADM_INST=`echo "$SADM_PN" |cut -d'.' -f1` ; export SADM_INST           # Script name without ext.
-SADM_TPID="$$"                             ; export SADM_TPID           # Script PID
-SADM_EXIT_CODE=0                           ; export SADM_EXIT_CODE      # Script Exit Return Code
-SADM_BASE_DIR=${SADMIN:="/sadmin"}         ; export SADM_BASE_DIR       # SADMIN Root Base Dir.
-SADM_LOG_TYPE="B"                          ; export SADM_LOG_TYPE       # 4Logger S=Scr L=Log B=Both
-SADM_LOG_APPEND="N"                        ; export SADM_LOG_APPEND     # Append to Existing Log ?
-SADM_MULTIPLE_EXEC="N"                     ; export SADM_MULTIPLE_EXEC  # Run many copy at same time
-[ -f ${SADM_BASE_DIR}/lib/sadmlib_std.sh ]    && . ${SADM_BASE_DIR}/lib/sadmlib_std.sh     
 
-# These variables are defined in sadmin.cfg file - You can also change them on a per script basis 
-SADM_SSH_CMD="${SADM_SSH} -qnp ${SADM_SSH_PORT}" ; export SADM_SSH_CMD  # SSH Command to Access Farm
-SADM_MAIL_TYPE=1                           ; export SADM_MAIL_TYPE      # 0=No 1=Err 2=Succes 3=All
-#SADM_MAX_LOGLINE=5000                       ; export SADM_MAX_LOGLINE   # Max Nb. Lines in LOG )
-#SADM_MAX_RCLINE=100                         ; export SADM_MAX_RCLINE    # Max Nb. Lines in RCH file
-#SADM_MAIL_ADDR="your_email@domain.com"      ; export ADM_MAIL_ADDR      # Email Address of owner
+
 #===================================================================================================
+# Setup SADMIN Global Variables and Load SADMIN Shell Library
 #
+    # TEST IF SADMIN LIBRARY IS ACCESSIBLE
+    if [ -z "$SADMIN" ]                                 # If SADMIN Environment Var. is not define
+        then echo "Please set 'SADMIN' Environment Variable to install directory." 
+             exit 1                                     # Exit to Shell with Error
+    fi
+    if [ ! -r "$SADMIN/lib/sadmlib_std.sh" ]            # SADM Shell Library not readable
+        then echo "SADMIN Library can't be located"     # Without it, it won't work 
+             exit 1                                     # Exit to Shell with Error
+    fi
+
+    # CHANGE THESE VARIABLES TO YOUR NEEDS - They influence execution of SADMIN standard library.
+    export SADM_VER='2.1'                               # Current Script Version
+    export SADM_LOG_TYPE="B"                            # Output goes to [S]creen [L]ogFile [B]oth
+    export SADM_LOG_APPEND="N"                          # Append Existing Log or Create New One
+    export SADM_LOG_HEADER="Y"                          # Show/Generate Header in script log (.log)
+    export SADM_LOG_FOOTER="Y"                          # Show/Generate Footer in script log (.log)
+    export SADM_MULTIPLE_EXEC="N"                       # Allow running multiple copy at same time ?
+    export SADM_USE_RCH="Y"                             # Generate entry in Return Code History .rch
+
+    # DON'T CHANGE THESE VARIABLES - They are used to pass information to SADMIN Standard Library.
+    export SADM_PN=${0##*/}                             # Current Script name
+    export SADM_INST=`echo "$SADM_PN" |cut -d'.' -f1`   # Current Script name, without the extension
+    export SADM_TPID="$$"                               # Current Script PID
+    export SADM_EXIT_CODE=0                             # Current Script Exit Return Code
+
+    # Load SADMIN Standard Shell Library 
+    . ${SADMIN}/lib/sadmlib_std.sh                      # Load SADMIN Shell Standard Library
+
+    # Default Value for these Global variables are defined in $SADMIN/cfg/sadmin.cfg file.
+    # But some can overriden here on a per script basis.
+    #export SADM_MAIL_TYPE=1                            # 0=NoMail 1=MailOnError 2=MailOnOK 3=Allways
+    #export SADM_MAIL_ADDR="your_email@domain.com"      # Email to send log (To Override sadmin.cfg)
+    #export SADM_MAX_LOGLINE=5000                       # When Script End Trim log file to 5000 Lines
+    #export SADM_MAX_RCLINE=100                         # When Script End Trim rch file to 100 Lines
+    #export SADM_SSH_CMD="${SADM_SSH} -qnp ${SADM_SSH_PORT} " # SSH Command to Access Server 
+#===================================================================================================
 
 
 
-# --------------------------------------------------------------------------------------------------
-#                               This Script environment variables
-# --------------------------------------------------------------------------------------------------
+
+#===================================================================================================
+# Scripts Variables 
+#===================================================================================================
 DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDebug Higher=+Verbose
-NFS_SERVER="batnas.maison.ca"               ; export NFS_SERVER         # Remote Backup Name
-NFS_DIR="/volume1/Linux_DR"                 ; export NFS_DIR            # Dir where Backup Go
 NFS_MOUNT="/mnt/nfs1"                       ; export NFS_MOUNT          # Local NFS Mount Point 
-REAR_COPY=2                                 ; export REAR_COPY          # Nb. of images to keep
-REAR_CFGFILE="/etc/rear/site.conf"          ; export REAR_CFGFILE       # Read Configuration file
+REAR_CFGFILE="/etc/rear/site.conf"          ; export REAR_CFGFILE       # ReaR Configuration file
 
 
+
+# --------------------------------------------------------------------------------------------------
+#       H E L P      U S A G E   A N D     V E R S I O N     D I S P L A Y    F U N C T I O N
+# --------------------------------------------------------------------------------------------------
+show_usage()
+{
+    printf "\n${SADM_PN} usage :"
+    printf "\n\t-d   (Debug Level [0-9])"
+    printf "\n\t-h   (Display this help message)"
+    printf "\n\t-v   (Show Script Version Info)"
+    printf "\n\n" 
+}
+
+show_version()
+{
+    printf "\n${SADM_PN} - Version $SADM_VER"
+    printf "\nSADMIN Shell Library Version $SADM_LIB_VER"
+    printf "\n$(sadm_get_osname) - Version $(sadm_get_osversion)"
+    printf " - Kernel Version $(sadm_get_kernel_version)"
+    printf "\n\n" 
+}
+
+# --------------------------------------------------------------------------------------------------
+# Update 'BACKUP_URL' line in /etc/rear/site.conf file, to have same NFS Server and MOunt Point 
+# found in $SADMIN/cfg/sadmin.cfg file.
+#       $SADM_REAR_NFS_SERVER               # ReaR NFS Server where backup will be stored
+#       $SADM_REAR_NFS_MOUNT_POINT          # ReaR Mount Point exported on the NFS Server
+# --------------------------------------------------------------------------------------------------
+update_site_conf()
+{
+    sadm_writelog "Updating 'BACKUP_URL' line in ReaR Site file ($REAR_CFGFILE) ..."
+    grep -v 'BACKUP_URL' $REAR_CFGFILE > $SADM_TMP_FILE2
+    newline="BACKUP_URL=\"nfs://${SADM_REAR_NFS_SERVER}/${SADM_REAR_NFS_MOUNT_POINT}/\"" 
+    echo $newline >>$SADM_TMP_FILE2
+    cp $SADM_TMP_FILE2 $REAR_CFGFILE
+    chmod 644 $REAR_CFGFILE
+    chown root.root $REAR_CFGFILE
+}
+
+
+
+
+
+    
 # --------------------------------------------------------------------------------------------------
 #                     Create the Rescue ISO and a tar file of the server
 # --------------------------------------------------------------------------------------------------
@@ -122,13 +197,13 @@ rear_housekeeping()
     if [ ! -d ${NFS_MOUNT} ] ; then mkdir ${NFS_MOUNT} ; chmod 775 ${NFS_MOUNT} ; fi
 
     # Mount the NFS Mount point 
-    sadm_writelog "Mounting the NFS Drive on $NFS_SERVER"
+    sadm_writelog "Mounting the NFS Drive on $SADM_REAR_NFS_SERVER"
     umount ${NFS_MOUNT} > /dev/null 2>&1
-    sadm_writelog "mount ${NFS_SERVER}:${NFS_DIR} ${NFS_MOUNT}"
-    mount ${NFS_SERVER}:${NFS_DIR} ${NFS_MOUNT} >>$SADM_LOG 2>&1
+    sadm_writelog "mount ${SADM_REAR_NFS_SERVER}:${SADM_REAR_NFS_MOUNT_POINT} ${NFS_MOUNT}"
+    mount ${SADM_REAR_NFS_SERVER}:${SADM_REAR_NFS_MOUNT_POINT} ${NFS_MOUNT} >>$SADM_LOG 2>&1
     if [ $? -ne 0 ]
         then RC=1
-             sadm_writelog "Mount of $NFS_DIR on NFS server $NFS_SERVER Failed"
+             sadm_writelog "Mount of $SADM_REAR_NFS_MOUNT_POINT on NFS server $SADM_REAR_NFS_SERVER Failed"
              sadm_writelog "Proces Aborted"
              umount ${NFS_MOUNT} > /dev/null 2>&1
              return 1
@@ -184,19 +259,19 @@ rear_housekeeping()
     fi
                     
     sadm_writelog " "
-    sadm_writelog "You choose to keep $REAR_COPY backup files on the NFS server"
+    sadm_writelog "You choose to keep $SADM_REAR_BACKUP_TO_KEEP backup files on the NFS server"
     sadm_writelog "Here is a list of ReaR backup and ISO on NFS Server for ${SADM_HOSTNAME}"
     #sadm_writelog "ls -1t ${REAR_NAME}*.iso | sort -r"
     ls -1t ${REAR_NAME}*.iso | sort -r | tee -a $SADM_LOG
     ls -1t ${REAR_NAME}*.gz  | sort -r | tee -a $SADM_LOG
 
-    COUNT_GZ=` ls -1t  ${REAR_NAME}*.gz  |sort -r |sed 1,${REAR_COPY}d | wc -l`  # Nb of GZ  to Del.
+    COUNT_GZ=` ls -1t  ${REAR_NAME}*.gz  |sort -r |sed 1,${SADM_REAR_BACKUP_TO_KEEP}d | wc -l`  # Nb of GZ  to Del.
     if [ "$COUNT_GZ" -ne 0 ]
         then sadm_writelog " "
              sadm_writelog "Number of backup file(s) to delete is $COUNT_GZ"
              sadm_writelog "List of backup file(s) that will be Deleted :"
-             ls -1t ${REAR_NAME}*.gz | sort -r| sed 1,${REAR_COPY}d | tee -a $SADM_LOG
-             ls -1t ${REAR_NAME}*.gz | sort -r| sed 1,${REAR_COPY}d | xargs rm -f >> $SADM_LOG 2>&1
+             ls -1t ${REAR_NAME}*.gz | sort -r| sed 1,${SADM_REAR_BACKUP_TO_KEEP}d | tee -a $SADM_LOG
+             ls -1t ${REAR_NAME}*.gz | sort -r| sed 1,${SADM_REAR_BACKUP_TO_KEEP}d | xargs rm -f >> $SADM_LOG 2>&1
              RC=$?
              if [ $RC -ne 0 ] ;then sadm_writelog "Problem deleting backup file(s)" ;FNC_ERROR=1; fi
              if [ $RC -eq 0 ] ;then sadm_writelog "Backup was deleted with success" ;fi
@@ -205,13 +280,13 @@ rear_housekeeping()
              sadm_writelog "We don't need to delete any backup file"
     fi
         
-    COUNT_ISO=`ls -1t  ${REAR_NAME}*.iso |sort -r |sed 1,${REAR_COPY}d | wc -l`  # Nb of ISO  to Del.
+    COUNT_ISO=`ls -1t  ${REAR_NAME}*.iso |sort -r |sed 1,${SADM_REAR_BACKUP_TO_KEEP}d | wc -l`  # Nb of ISO  to Del.
     if [ "$COUNT_ISO" -ne 0 ]
         then sadm_writelog " "
              sadm_writelog "Number of ISO file(s) to delete is $COUNT_ISO"
              sadm_writelog "List of ISO file(s) that will be Deleted :"
-             ls -1t ${REAR_NAME}*.iso | sort -r| sed 1,${REAR_COPY}d | tee -a $SADM_LOG
-             ls -1t ${REAR_NAME}*.iso | sort -r| sed 1,${REAR_COPY}d | xargs rm -f >> $SADM_LOG 2>&1
+             ls -1t ${REAR_NAME}*.iso | sort -r| sed 1,${SADM_REAR_BACKUP_TO_KEEP}d | tee -a $SADM_LOG
+             ls -1t ${REAR_NAME}*.iso | sort -r| sed 1,${SADM_REAR_BACKUP_TO_KEEP}d | xargs rm -f >> $SADM_LOG 2>&1
              RC=$?
              if [ $RC -ne 0 ] ; then sadm_writelog "Problem deleting ISO file(s)"; FNC_ERROR=1; fi
              if [ $RC -eq 0 ] ; then sadm_writelog "ISO file(s) deleted with success" ; fi
@@ -244,15 +319,39 @@ rear_housekeeping()
 # --------------------------------------------------------------------------------------------------
 #                                    Script Start HERE
 # --------------------------------------------------------------------------------------------------
-    sadm_start                                                          # Init Env Dir & RC/Log File
+    
+    # If you want this script to be run only by 'root'.
     if ! [ $(id -u) -eq 0 ]                                             # If Cur. user is not root 
-        then sadm_writelog "This script must be run by the ROOT user"   # Advise User Message
-             sadm_writelog "Process aborted"                            # Abort advise message
-             sadm_stop 1                                                # Close and Trim Log
-             exit 1                                                     # Exit To O/S
+        then printf "\nThis script must be run by the 'root' user"      # Advise User Message
+             printf "\nTry sudo %s" "${0##*/}"                          # Suggest using sudo
+             printf "\nProcess aborted\n\n"                             # Abort advise message
+             exit 1                                                     # Exit To O/S with error
     fi
 
+    # Switch for Help Usage (-h), Show Script Version (-v) or Activate Debug Level (-d[1-9])
+    while getopts "hvd:" opt ; do                                       # Loop to process Switch
+        case $opt in
+            d) DEBUG_LEVEL=$OPTARG                                      # Get Debug Level Specified
+               ;;                                                       # No stop after each page
+            h) show_usage                                               # Show Help Usage
+               exit 0                                                   # Back to shell
+               ;;
+            v) show_version                                             # Show Script Version Info
+               exit 0                                                   # Back to shell
+               ;;
+           \?) printf "\nInvalid option: -$OPTARG"                      # Invalid Option Message
+               show_usage                                               # Display Help Usage
+               exit 1                                                   # Exit with Error
+               ;;
+        esac                                                            # End of case
+    done                                                                # End of while
+    if [ $DEBUG_LEVEL -gt 0 ] ; then printf "\nDebug activated, Level ${DEBUG_LEVEL}" ; fi
+    
+
+    sadm_start                                                          # Start Using SADM Tools
+    if [ $? -ne 0 ] ; then sadm_stop 1 ; exit 1 ;fi                     # If Problem during init
     # Check if REAR is not installed - Abort Process 
+
     REAR=`${SADM_WHICH} rear`
     if ${SADM_WHICH} rear >/dev/null 2>&1                               # command is found ?
         then REAR=`${SADM_WHICH} rear`                                  # Store Path of command
@@ -267,6 +366,7 @@ rear_housekeeping()
              sadm_writelog "The backup will not run - Job Aborted"      # Warn User - No Backup
              sadm_stop 1                                                # Upd. RCH File & Trim Log 
              exit 1                                                     # Exit With Global Err (0/1)
+        else update_site_conf                                           # Update ReaR site.conf 
     fi
     
     # Remove default crontab job - So we can decide otherwise when we run rear from this script
