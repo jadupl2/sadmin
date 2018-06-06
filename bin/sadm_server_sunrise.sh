3907a0ff57037124a3dafeb6c67ed262627f2c59
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-06-06 15:39:13 -0400

    v2.0 Restructure Code, Added Comments & Change for New SADMIN Libr.

--- bin/sadm_server_sunrise.sh
+++ bin/sadm_server_sunrise.sh
@@ -1,20 +1,27 @@
 #! /usr/bin/env sh
 # --------------------------------------------------------------------------------------------------
-#   Author   :  Jacques Duplessis
-#   Title    :  sadm_server_sunrise.sh
-#   Synopsis :  Start Of Day script, run daily in the morning, so it is finish when you arrive
-#               at work.
-#               It run multiple script that rsync stat. and info file from client to this server
-#   Version  :  1.6
-#   Date     :  11 December 2016
-#   Requires :  sh
+#   Author      :  Jacques Duplessis
+#   Title       :  sadm_server_sunrise.sh
+#   Synopsis    :  Daily early morning script, that retrieve data from servers & Update/Backup DB & NetInfo
+#   Description :  Run Once Daily early around 5-6 am, it run multiple scripts before you start 
+#                  your working day.
+#                       Run Multiple Scripts :
+#                           1)  housekeeping_server
+#                           2)  daily_data_collection
+#                           3)  database_update
+#                           4)  nmon_rrd_update
+#                           5)  subnet_lookup
+#                           6)  backupdb
+#   Version     :  1.6
+#   Date        :  11 December 2016
+#   Requires    :  sh & sadmlib.sh 
 #
 #   Copyright (C) 2016 Jacques Duplessis <duplessis.jacques@gmail.com>
 #
 #   The SADMIN Tool is free software; you can redistribute it and/or modify it under the terms
 #   of the GNU General Public License as published by the Free Software Foundation; either
 #   version 2 of the License, or (at your option) any later version.
-
+#
 #   SADMIN Tools are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 #   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #   See the GNU General Public License for more details.
@@ -22,16 +29,17 @@
 #   You should have received a copy of the GNU General Public License along with this program.
 #   If not, see <http://www.gnu.org/licenses/>.
 # --------------------------------------------------------------------------------------------------
-# Enhancements/Corrections Version Log
-#   V1.6 Cosmetics and Refresh changes 
-#   V1.7 Added execution of MySQL Database Python Update
-# 2018_01_25 JDuplessis
-#   V1.8 Added execution of the update of Performance RRD based on nmon content of each server
-#        Change name from sadm_sod_server.sh to sadm_server_start_of_day.sh
-# 2018_02_04 JDuplessis
-#   V1.9 Add Mysql Backup & Change name from sadm_server_start_of_day.sh to sadm_server_sunrise.sh
-# 2018_02_11 JDuplessis
-#   V1.10 Change message when succeeded to run script
+# Change Log
+#
+# 2016_12_12    v1.6 Cosmetics and Refresh changes 
+# 2016_12_14    v1.7 Added execution of MySQL Database Python Update
+# 2018_01_25    v1.8 Added update of Performance RRD based on nmon content of each server
+#                    Change name from sadm_sod_server.sh to sadm_server_start_of_day.sh
+# 2018_02_04    v1.9 Add Mysql Backup
+#                    Change name from sadm_server_start_of_day.sh to sadm_server_sunrise.sh
+# 2018_02_11    v1.10 Change message when succeeded to run script
+# 2018_06_06    v2.0 Restructure Code, Added Comments & Change for New SADMIN Libr.
+#
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPTE LE ^C
 #set -x
@@ -46,7 +54,7 @@ if [ -z "$SADMIN" ] ;then echo "Please assign SADMIN Env. Variable to install di
 if [ ! -r "$SADMIN/lib/sadmlib_std.sh" ] ;then echo "SADMIN Library can't be located"   ;exit 1 ;fi
 #
 # YOU CAN CHANGE THESE VARIABLES - They Influence the execution of functions in SADMIN Library
-SADM_VER='1.10'                             ; export SADM_VER            # Your Script Version
+SADM_VER='2.0'                             ; export SADM_VER            # Your Script Version
 SADM_LOG_TYPE="B"                          ; export SADM_LOG_TYPE       # S=Screen L=LogFile B=Both
 SADM_LOG_APPEND="N"                        ; export SADM_LOG_APPEND     # Append to Existing Log ?
 SADM_MULTIPLE_EXEC="N"                     ; export SADM_MULTIPLE_EXEC  # Run many copy at same time
@@ -81,25 +89,34 @@ DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDe
 
 
 
-
 # --------------------------------------------------------------------------------------------------
-#                                Script Start HERE
+#       H E L P      U S A G E   A N D     V E R S I O N     D I S P L A Y    F U N C T I O N
 # --------------------------------------------------------------------------------------------------
-    sadm_start                                                          # Init Env Dir & RC/Log File
-    if [ $? -ne 0 ] ; then sadm_stop 1 ; exit 1 ;fi                     # Exit if Problem 
-     if [ "$(sadm_get_fqdn)" != "$SADM_SERVER" ]                        # Only run on SADMIN 
-        then sadm_writelog "Script can run only on SADMIN server (${SADM_SERVER})"
-             sadm_writelog "Process aborted"                            # Abort advise message
-             sadm_stop 1                                                # Close and Trim Log
-             exit 1                                                     # Exit To O/S
-    fi
-    if ! [ $(id -u) -eq 0 ]                                             # If Cur. user is not root 
-        then sadm_writelog "Script can only be run by the 'root' user"  # Advise User Message
-             sadm_writelog "Process aborted"                            # Abort advise message
-             sadm_stop 1                                                # Close and Trim Log
-             exit 1                                                     # Exit To O/S
-    fi
+show_usage()
+{
+    printf "\n${SADM_PN} usage :"
+    printf "\n\t-d   (Debug Level [0-9])"
+    printf "\n\t-h   (Display this help message)"
+    printf "\n\t-v   (Show Script Version Info)"
+    printf "\n\n" 
+}
+show_version()
+{
+    printf "\n${SADM_PN} - Version $SADM_VER"
+    printf "\nSADMIN Shell Library Version $SADM_LIB_VER"
+    printf "\n$(sadm_get_osname) - Version $(sadm_get_osversion)"
+    printf " - Kernel Version $(sadm_get_kernel_version)"
+    printf "\n\n" 
+}
 
+
+#===================================================================================================
+# Main Process (Used to run script on current server)
+#===================================================================================================
+main_process()
+{
+    sadm_writelog "Starting Main Process ... "                          # Inform User Starting Main
+    
     # Once a day - Delete old rch and log files & chown+chmod on SADMIN Server
     SCMD="${SADM_BIN_DIR}/sadm_housekeeping_server.sh"
     sadm_writelog " " ; sadm_writelog "Running $SCMD ..."
@@ -160,6 +177,49 @@ DEBUG_LEVEL=0                               ; export DEBUG_LEVEL        # 0=NoDe
         else sadm_writelog "[SUCCESS] Running $SCMD"                    # Advise user it's OK
     fi
     
-    # Go Write Log Footer - Send email if needed - Trim the Log - Update the Recode History File
+    return $SADM_EXIT_CODE                                              # Return No Error to Caller
+}
+
+
+
+# --------------------------------------------------------------------------------------------------
+#                                Script Start HERE
+# --------------------------------------------------------------------------------------------------
+    sadm_start                                                          # Init Env Dir & RC/Log File
+    if [ $? -ne 0 ] ; then sadm_stop 1 ; exit 1 ;fi                     # Exit if Problem 
+     if [ "$(sadm_get_fqdn)" != "$SADM_SERVER" ]                        # Only run on SADMIN 
+        then sadm_writelog "Script can run only on SADMIN server (${SADM_SERVER})"
+             sadm_writelog "Process aborted"                            # Abort advise message
+             sadm_stop 1                                                # Close and Trim Log
+             exit 1                                                     # Exit To O/S
+    fi
+    if ! [ $(id -u) -eq 0 ]                                             # If Cur. user is not root 
+        then sadm_writelog "Script can only be run by the 'root' user"  # Advise User Message
+             sadm_writelog "Process aborted"                            # Abort advise message
+             sadm_stop 1                                                # Close and Trim Log
+             exit 1                                                     # Exit To O/S
+    fi
+
+    # Switch for Help Usage (-h), Show Script Version (-v) or Activate Debug Level (-d[1-9])
+    while getopts "hvd:" opt ; do                                       # Loop to process Switch
+        case $opt in
+            d) DEBUG_LEVEL=$OPTARG                                      # Get Debug Level Specified
+               ;;                                                       # No stop after each page
+            h) show_usage                                               # Show Help Usage
+               exit 0                                                   # Back to shell
+               ;;
+            v) show_version                                             # Show Script Version Info
+               exit 0                                                   # Back to shell
+               ;;
+           \?) printf "\nInvalid option: -$OPTARG"                      # Invalid Option Message
+               show_usage                                               # Display Help Usage
+               exit 1                                                   # Exit with Error
+               ;;
+        esac                                                            # End of case
+    done                                                                # End of while
+    if [ $DEBUG_LEVEL -gt 0 ] ; then printf "\nDebug activated, Level ${DEBUG_LEVEL}" ; fi
+    
+    main_process                                                        # Main Process
+    SADM_EXIT_CODE=$?                                                   # Save Nb. Errors in process
     sadm_stop $SADM_EXIT_CODE                                           # Upd. RCH File & Trim Log 
     exit $SADM_EXIT_CODE                                                # Exit With Global Err (0/1)
