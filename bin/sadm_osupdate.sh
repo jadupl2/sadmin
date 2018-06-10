1a24a048d3d254b466ee6a94355e9292ec8bb18d
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-06-10 19:18:26 -0400

    v3.7 Switch back to old name

--- bin/sadm_osupdate.sh
+++ bin/sadm_osupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # --------------------------------------------------------------------------------------------------
 #   Author:     Jacques Duplessis
-#   Title:      sadm_client_osupdate.sh _ Linux O/S Update script
+#   Title:      sadm_osupdate.sh _ Linux O/S Update script
 #   Synopsis:   This script is used to update the Linux OS Platform
 #               Support Redhat/Centos v3,4,5,6,7 - Ubuntu - Debian V7,8 - Raspbian V7,8 - Fedora
 #   Update  :   March 2015 -  J.Duplessis
@@ -23,6 +23,7 @@
 # 2017_12_10    v3.4 No longer Support Redhat/CentOS 3 and 4
 # 2018_06_05    v3.5 Adapt to new SADMIN Libr.
 # 2018_06_09    v3.6 Change name of this script from sadm_osupdate_client to sadm_client_osupdate
+# 2018_06_10    v3.7 Switch back to old name 
 # --------------------------------------------------------------------------------------------------
 #set -x
 
@@ -44,7 +45,7 @@
     fi
 
     # CHANGE THESE VARIABLES TO YOUR NEEDS - They influence execution of SADMIN standard library.
-    export SADM_VER='3.6'                               # Current Script Version
+    export SADM_VER='3.7'                               # Current Script Version
     export SADM_LOG_TYPE="B"                            # Output goes to [S]creen [L]ogFile [B]oth
     export SADM_LOG_APPEND="N"                          # Append Existing Log or Create New One
     export SADM_LOG_HEADER="Y"                          # Show/Generate Header in script log (.log)
