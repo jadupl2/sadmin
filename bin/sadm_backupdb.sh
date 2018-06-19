cf71f54639b654b24c2f1ed379a57c82383382d2
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-06-19 11:16:36 -0400

    V1.8 Default option is to Compress Backup - Add -u to do uncompress backup

--- bin/sadm_backupdb.sh
+++ bin/sadm_backupdb.sh
@@ -2,7 +2,7 @@
 # --------------------------------------------------------------------------------------------------
 #   Author      :   Jacques Duplessis
 #   Title       :   sadm_server_backupdb.sh
-#   Synopsis    :   MySQL SADMIN Database Backup
+#   Synopsis    :   MariaDB/MySQL Database Backup
 #   Version     :   1.0
 #   Date        :   4 January 2018
 #   Requires    :   sh and SADM Library
@@ -34,6 +34,7 @@
 #                   can compress backup.
 # 2018_06_03    V1.6 Small Ameliorations and corrections
 # 2018_06_11    V1.7 Change name to sadm_backupsd.sh
+# 2018_06_19    V1.8 Default option is to Compress Backup - Add -u to do uncompress backup
 #
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPT The Control-C
@@ -56,7 +57,7 @@ trap 'sadm_stop 0; exit 0' 2                                            # INTERC
     fi
 
     # CHANGE THESE VARIABLES TO YOUR NEEDS - They influence execution of SADMIN standard library.
-    export SADM_VER='1.7'                               # Current Script Version
+    export SADM_VER='1.8'                               # Current Script Version
     export SADM_LOG_TYPE="B"                            # Output goes to [S]creen [L]ogFile [B]oth
     export SADM_LOG_APPEND="N"                          # Append Existing Log or Create New One
     export SADM_LOG_HEADER="Y"                          # Show/Generate Header in script log (.log)
@@ -412,8 +413,8 @@ main_process()
 
     # Switch for Help (-h), Debug Level (-d[1-9]), Compress Backup (-c), Database to Backup (-n)----
     BACKUP_NAME="all"                                                   # DB to Backup Default=None
-    COMPRESS_BACKUP="N"                                                 # Gzip Backup OFF by Default
-    while getopts "had:n:c" opt ; do                                    # Loop to process Switch
+    COMPRESS_BACKUP="Y"                                                 # Compress Backup by Default
+    while getopts "had:n:u" opt ; do                                    # Loop to process Switch
         case $opt in
             d) DEBUG_LEVEL=$OPTARG                                      # Get Debug Level Specified
                ;;                                                       # No stop after each page
@@ -421,7 +422,7 @@ main_process()
                ;;
             a) BACKUP_NAME="all"                                        # Backup all database 
                ;;
-            c) COMPRESS_BACKUP="Y"                                      # Compress Backup is now ON
+            u) COMPRESS_BACKUP="N"                                      # Backup Uncompress 
                ;;
             h) help_usage                                               # Display Help Usage
                sadm_stop 0                                              # Close the shop
