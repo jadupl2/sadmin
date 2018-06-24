fbd3b98da0ce40d3e38f1f8a8b53775bee9f9e5b
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-06-24 11:29:20 -0400

    SADMIN Default to /opt/sadmin now

--- setup/etc/sadmin.sh
+++ setup/etc/sadmin.sh
@@ -5,16 +5,14 @@
 # Author    	:	J.Duplessis
 # Date      	:	2018-04-01
 # Requires  	:	bash shell
-# Category  	:	System Administration
-# SCCS-Id.  	:	@(#) sadmin.sh V2
 # Synopsis	:   	This script is executed when users login system wide & when system start 
 #                   Do not modify this, it could be overwritten by new release.
 ####################################################################################################
 #
 # SADMIN root Directory
-SADMIN=/sadmin
+SADMIN=/opt/sadmin
 export SADMIN
 #
-# Add SADMIN/bin to PATH
+# Add SADMIN bin and usr/bin to PATH
 export PATH=$PATH:${SADMIN}/bin:${SADMIN}/usr/bin
 #
