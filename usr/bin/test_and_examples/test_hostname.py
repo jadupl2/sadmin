7a48b68028fa031cce1497eff99dc3e6a252a0a1
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-07-16 09:26:12 -0400

    Change for testing

--- usr/bin/test_and_examples/test_hostname.py
+++ usr/bin/test_and_examples/test_hostname.py
@@ -1,6 +1,21 @@
 #! /usr/bin/env python3
 import os, sys, socket
 
+
+
+sadm = socket.gethostbyname('raspi3.maison.ca')
+print ("sadm=%s\n" % (sadm))
+digit1=sadm.split('.')[0]                                               # 1st Digit=127 = Invalid
+print ("digit1=%s\n" % (digit1))
+if (digit1 == "127"):
+    print ("SADMIN server name can't resolve to localhost (%s)" %(sadm))
+    print ("SADMIN clients would not be able to get to the SADMIN Server")
+    print ("SADMIN Server name must resolve to an IP other than in 127.0.0.0/24 subnet")
+    print ("You may need to press CTRL-C to abort installation and correct the situation")
+    print ("Once resolve, just execute the setup program again")
+
+
+
 sfqdn = socket.getfqdn()
 hostip = socket.gethostbyname(sfqdn)
 uhost = os.uname()[1]
