844b52f1cc5ef8a6971402d9d5136acd85ed8eb7
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-09-14 12:44:41 -0400

    v2.5 Added alert group, alert slack and alert History file to SADM

--- usr/bin/sadmin_to_psadmin.sh
+++ usr/bin/sadmin_to_psadmin.sh
@@ -37,7 +37,8 @@
 # 2018_07_09    v2.1 Copy .version & .versum from psadmin to sadmin at end so git = release version
 # 2018_07_18    v2.2 Copy system monitor script template, nmon monitor & service restart in PSADMIN
 # 2018_08_25    v2.3 Added .gitkeep file to empty directory so they exist in clone image & tgz file.
-#@2018_08_26    v2.4 Rewritten to use rsync between sadmin to psadmin, cause want to use git (.git).
+# 2018_08_26    v2.4 Rewritten to use rsync between sadmin to psadmin, cause want to use git (.git).
+#@2018_09_14    v2.5 Added alert group, alert slack and alert History file to SADM
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPT The Control-C
 #set -x
@@ -59,13 +60,13 @@ trap 'sadm_stop 0; exit 0' 2                                            # INTERC
     fi
 
     # CHANGE THESE VARIABLES TO YOUR NEEDS - They influence execution of SADMIN standard library.
-    export SADM_VER='2.4'                               # Current Script Version
+    export SADM_VER='2.5'                               # Current Script Version
     export SADM_LOG_TYPE="B"                            # Writelog goes to [S]creen [L]ogFile [B]oth
     export SADM_LOG_APPEND="N"                          # Append Existing Log or Create New One
     export SADM_LOG_HEADER="Y"                          # Show/Generate Script Header
     export SADM_LOG_FOOTER="Y"                          # Show/Generate Script Footer 
     export SADM_MULTIPLE_EXEC="N"                       # Allow running multiple copy at same time ?
-    export SADM_USE_RCH="Y"                             # Generate Entry in Result Code History file
+    export SADM_USE_RCH="N"                             # Generate Entry in Result Code History file
 
     # DON'T CHANGE THESE VARIABLES - They are used to pass information to SADMIN Standard Library.
     export SADM_PN=${0##*/}                             # Current Script name
@@ -78,7 +79,8 @@ trap 'sadm_stop 0; exit 0' 2                                            # INTERC
 
     # Default Value for these Global variables are defined in $SADMIN/cfg/sadmin.cfg file.
     # But some can overriden here on a per script basis.
-    #export SADM_ALERT_TYPE=1                            # 0=None 1=AlertOnErr 2=AlertOnOK 3=Allways
+    export SADM_ALERT_TYPE=1                            # 0=None 1=AlertOnErr 2=AlertOnOK 3=Allways
+    export SADM_ALERT_GROUP="default"                   # AlertGroup Used to Alert (alert_group.cfg)
     #export SADM_MAIL_ADDR="your_email@domain.com"      # Email to send log (To Override sadmin.cfg)
     #export SADM_MAX_LOGLINE=1000                       # When Script End Trim log file to 1000 Lines
     #export SADM_MAX_RCLINE=125                         # When Script End Trim rch file to 125 Lines
@@ -406,10 +408,22 @@ main_process()
     run_oscommand "cp ${SADM_CFG_DIR}/.sadmin.service ${PSCFG}"
     run_oscommand "chmod 644 ${PSCFG}/.sadmin.service"
     run_oscommand "chown sadmin.sadmin ${PSCFG}/.sadmin.service"
-        #
+    #
     run_oscommand "cp ${SADM_CFG_DIR}/.sadmin.rc ${PSCFG}"
     run_oscommand "chmod 644 ${PSCFG}/.sadmin.rc"
     run_oscommand "chown sadmin.sadmin ${PSCFG}/.sadmin.rc"
+    #
+    run_oscommand "cp ${SADM_CFG_DIR}/.alert_group.cfg ${PSCFG}"
+    run_oscommand "chmod 644 ${PSCFG}/.alert_group.cfg"
+    run_oscommand "chown sadmin.sadmin ${PSCFG}/.alert_group.cfg"
+    #
+    run_oscommand "cp ${SADM_CFG_DIR}/.alert_slack.cfg ${PSCFG}"
+    run_oscommand "chmod 644 ${PSCFG}/.alert_slack.cfg"
+    run_oscommand "chown sadmin.sadmin ${PSCFG}/.alert_slack.cfg"
+    #
+    run_oscommand "cp ${SADM_CFG_DIR}/.alert_history.txt ${PSCFG}"
+    run_oscommand "chmod 644 ${PSCFG}/.alert_history.txt"
+    run_oscommand "chown sadmin.sadmin ${PSCFG}/.alert_history.txt"
 
 
     # COPY SADMIN LICENCE / README FILE------------------------------------------------------------
