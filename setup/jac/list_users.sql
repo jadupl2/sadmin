765ae0604c2956d8edaf5bd68a33a8f1f5821ffd
Jacques Duplessis - holmes <duplessis.jacques@gmail.com>
2018-09-29 12:00:12 -0400

    Adjustments for WIP

--- setup/jac/list_users.sql
+++
@@ -1,1 +0,0 @@
-select User, Host, Password  from mysql.user;
--- setup/jac/useradd2.sql
+++
@@ -1,8 +0,0 @@
-use mysql;
-flush privileges;
-CREATE USER 'sadmin'@'localhost' IDENTIFIED BY 'Nimdas17';
-grant all privileges on sadmin.* to 'sadmin'@'localhost';
-CREATE USER 'squery'@'localhost' IDENTIFIED BY 'Squery18';
-grant select, show view on sadmin.* to 'squery'@'localhost';
-grant all privileges on *.* to 'root'@'localhost' identified by 'jacques';
-flush privileges;
