095b3447e01527ecccf253c27d6b75cb95a05567
Jacques Duplessis <duplessis.jacques@gmail.com>
2019-03-23 11:15:38 -0400

    Minor, Typo, Mess update

--- bin/sadm_template.py
+++ bin/sadm_template.py
@@ -24,14 +24,8 @@
 # --------------------------------------------------------------------------------------------------
 # 
 # Version Change Log 
-# 2017_12_01 V1.0 Initial Version
-# 2018_05_14 V1.7 Add USE_RCH Variabe to use or not the [R]eturn [C]ode [H]istory File
-# 2018_05_15 V1.8 Add log_header and log_footer to produce or not the log Header and Footer.
-# 2018_05_20 V1.9 Restructure the code and added comments
-# 2018_05_26 V2.0 Revisited - Align with New Version of SADMIN Library
-# 2018_10_02 V2.1 Add Debug variable to get more verbosity 
-# 2018_11_07 V2.2 Use Database by Default (Only on SADMIN server)
-#@2018_11_09 V2.3 Database Connection Error Improve
+#
+# 2019_MM_DD New: v1.0 Initial Version
 # 
 # --------------------------------------------------------------------------------------------------
 #
@@ -71,7 +65,7 @@ def setup_sadmin():
     st = sadm.sadmtools()                       # Create SADMIN Tools Instance (Setup Dir.,Var,...)
 
     # Change these values to your script needs.
-    st.ver              = "2.3"                 # Current Script Version
+    st.ver              = "1.0"                 # Current Script Version
     st.multiple_exec    = "N"                   # Allow running multiple copy at same time ?
     st.log_type         = 'B'                   # Output goes to [S]creen [L]ogFile [B]oth
     st.log_append       = False                 # Append Existing Log or Create New One
