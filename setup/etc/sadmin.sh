2e972ce54e210de09ba56ecc3fe2c0856c15ef9b
Jacques Duplessis <duplessis.jacques@gmail.com>
2019-04-08 12:23:28 -0400

    v2.2 Small Change - SADMIN on one line.

--- setup/etc/sadmin.sh
+++ setup/etc/sadmin.sh
@@ -1,17 +1,16 @@
 #! /bin/sh
 ####################################################################################################
 # Shellscript   :   sadmin.sh
-# Version       :   2.1
+# Version       :   2.2
 # Author        :   sadmin.ca
-# Date          :   2018-04-01
+# Date          :   2019-04-08
 # Requires      :   bash shell
-# Synopsis      :   This script is executed when users login system wide & when system start 
+# Synopsis      :   This script is executed when users login system wide.
 #                   Do not modify this, it could be overwritten by new release.
 ####################################################################################################
 #
 # SADMIN root Directory
-SADMIN=/opt/sadmin
-export SADMIN
+export SADMIN=/opt/sadmin
 #
 # Add SADMIN bin and usr/bin to PATH
 export PATH=$PATH:${SADMIN}/bin:${SADMIN}/usr/bin
