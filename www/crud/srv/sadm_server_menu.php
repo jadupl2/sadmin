a38ccde886d1d724b9b15eebbb190627e9799348
Jacques Duplessis <duplessis.jacques@gmail.com>
2019-08-18 11:37:00 -0400

    v1.4 Add ReaR Backup in menu.

--- www/crud/srv/sadm_server_menu.php
+++ www/crud/srv/sadm_server_menu.php
@@ -30,6 +30,7 @@
 #       V1.0 Initial version - Server Edit Menu to Split Server Table Edition Add lot of comments in code and enhance code performance 
 #@2019_01_11 Update: v1.2 Add menu item for updating backup schedule,
 #@2019_07_25 Update: v1.3 Minor modification to page layout.
+#@2019_08_18 Update: v1.4 Add ReaR Backup in menu.
 # ==================================================================================================
 #
 # REQUIREMENT COMMON TO ALL PAGE OF SADMIN SITE
@@ -40,40 +41,58 @@ require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmPageHeader.php');     # <head>
 
 
 ?>
-<style>
-a { color: #E95420; } /* CSS link color */
+<style media="screen" type="text/css">
+
 /* Menu Frame */
 .menu {
+    background-color:   #3b3b3b;
+    color           :   #f9f4be;   
     font-family     :   Verdana, Geneva, sans-serif;
-    background-color:   #2d3139;
-    color           :    white;
-    margin-left     :   auto;
-    margin-right    :   auto;
+    font-size       :   1.0em;
     width           :   45%;
-    border          :   1px solid #000000;
+    margin          :   0 auto;
     text-align      :   left;
-    padding         :   2%;
-    border-width    :   10px;
-    border-style    :   solid;
-    border-color    :   #124f44;;
-    border-radius   :   10px;
+    border          :   2px solid #000000;   border-width : 1px;     border-style : solid;   
+    border-color    :   #000000;             border-radius: 10px;
     line-height     :   1.7;    
 }
+
+ /* unvisited link */
+ a:link {
+  color: white;
+}
+
+/* visited link */
+a:visited {
+  color: green;
+}
+
+/* mouse over link */
+a:hover {
+  color: hotpink;
+}
+
+/* selected link */
+a:active {
+  color: blue;
+} 
+
+
 /* Attribute for Column Name at the left of the form */
 .menu_item {
     font-size       :   14px;
-    background-color:   #2d3139;
+    background-color:   #3b3b3b;
     /* margin-left     :   40px; */
     margin-left     :   auto;
     margin-right     :   auto;
     color           :    white;
     margin-bottom   :   5px;
     font-weight     :   bold;    
-    width           :   55%;
-    /* a:link { color: #70DB93; }
-    a:visited { color: #70DB93; }
-    a:hover { color: #70DB93; }
-    a:active { color: #70DB93; } */
+    width           :   70%;
+    /* a:link { color: #ffffff; }
+    a:visited { color: #ffffff; }
+    a:hover { color: #ffffff; }
+    a:active { color: #ffffff; }  */
 }
 </style>
 
@@ -88,37 +107,45 @@ require_once ($_SERVER['DOCUMENT_ROOT'].'/crud/srv/sadm_server_common.php');
 #===================================================================================================
 #
 $DEBUG          = False ;                                               # Debug Activated True/False
-$SVER           = "1.3" ;                                               # Current version number
+$SVER           = "1.4" ;                                               # Current version number
 $URL_MAIN       = '/crud/srv/sadm_server_main.php';                     # Maintenance Main Page URL
 $URL_UPDATE     = '/crud/srv/sadm_server_update.php';                   # Update Page URL
 $URL_OSUPDATE   = '/crud/srv/sadm_server_osupdate.php';                 # O/S Update Page URL
 $URL_BACKUP     = '/crud/srv/sadm_server_backup.php';                   # O/S Update Page URL
+$URL_REAR       = '/crud/srv/sadm_server_rear_backup.php';              # ReaR backup schedule page
 $URL_HOME       = '/index.php';                                         # Site Main Page
 $CREATE_BUTTON  = False ;                                               # Don't Show Create Button
+$URL_MENU       = "/crud/srv/sadm_server_menu.php";                     # CRUD Server Menu URL
 
 
 // ================================================================================================
 //                      DISPLAY SERVER UPDATE MENU 
 // ================================================================================================
 function display_menu($wkey) {
-    global $URL_UPDATE, $URL_OSUPDATE, $URL_BACKUP, $URL_MAIN;
-
+    global $URL_UPDATE, $URL_OSUPDATE, $URL_BACKUP, $URL_MAIN, $URL_MENU, $URL_REAR;
+    echo "\n<br><br>";
     echo "\n\n<div class='menu'>\n";                                    # Start Menu
+    echo "\n<br>";
+
     echo "\n<div class='menu_item'>\n";                                 # Start Menu Item
     echo "\n<p>";
-    echo "\n<a href='" . $URL_UPDATE . "?sel=" . $wkey ; 
-    echo "'>Edit static information</a></p>";
+    echo "\n<a href='" . $URL_UPDATE . "?sel=" . $wkey . "&back=" . $URL_MENU ; 
+    echo "'>System static information</a></p>";
     echo "\n<p>";
-    echo "\n<a href='" . $URL_OSUPDATE . "?sel=" . $wkey ;
-    echo "'>Edit O/S update schedule</a></p>";
+    echo "\n<a href='" . $URL_OSUPDATE . "?sel=" . $wkey . "&back=" . $URL_MENU ;
+    echo "'>Operating system update schedule</a></p>";
     echo "\n<p>";
-    echo "\n<a href='" . $URL_BACKUP . "?sel=" . $wkey ;
-    echo "'>Edit backup schedule</a></p>";
+    echo "\n<a href='" . $URL_BACKUP . "?sel=" . $wkey . "&back=" . $URL_MENU ;
+    echo "'>Backup schedule</a></p>";
+    echo "\n<p>";
+    echo "\n<a href='" . $URL_REAR . "?sel=" . $wkey . "&back=" . $URL_MENU ;
+    echo "'>ReaR backup schedule</a></p>";
     echo "\n<br>";
     echo "\n<p>\n<a href='" . $URL_MAIN . "'>Back to system list</a></p>";
     echo "\n</div>";                                                    # << End of menu_item
-    echo "\n</div>";                                                    # << End of menu
-    echo "\n<br>\n\n";                                                  # Blank Lines
+    echo "\n<br>";
+
+    echo "\n</div>\n<br>\n\n";                                          # End of Menu Div.
 }
 
 
@@ -126,7 +153,6 @@ function display_menu($wkey) {
 # Page Process Start Here
 # ==================================================================================================
 
-    display_std_heading("Home","Update Server Menu","","",$SVER);       # Display Content Heading
 
     if (isset($_GET['sel']) && !empty($_GET['sel'])) {                  # If Key Rcv and not Blank   
         $wkey = $_GET['sel'];                                           # Save Key Rcv to Work Key
@@ -154,9 +180,15 @@ function display_menu($wkey) {
         echo "<script>location.replace(" .$URL_MAIN. ");</script>";
         exit ; 
     }
-    $title="Update information of '" . $wkey . "." . $row['srv_domain'] . "' system";
-    echo "<h1><center><i><strong>" . $title . "</strong></i></center></h1>";
+
+    # DISPLAY SCREEN HEADING    
+    $title1="Update System Information Menu";
+    $title2="'" . $row['srv_name'] . "." . $row['srv_domain'] . "'";
+    display_lib_heading("NotHome","$title1","$title2",$SVER);           # Display Content Heading
+    #
+    # Show Menu 
     display_menu($wkey);                                                # Display Form Default Value
+    #
     echo "\n<br>";                                                      # Blank Line After Button
     std_page_footer($con)                                               # Close MySQL & HTML Footer
 ?>
