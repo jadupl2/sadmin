8d44bfc3daa106ae46b1f8cdbf9266a63d690e0d
Jacques Duplessis <duplessis.jacques@gmail.com>
2019-09-23 09:14:17 -0400

    v2.7 Add System Distribution logo & version

--- www/view/srv/sadm_view_servers.php
+++ www/view/srv/sadm_view_servers.php
@@ -36,6 +36,7 @@
 #@ 2019_01_14 Feature: v2.4 See Server Model and Serial No. when mouse over server name.
 #@ 2019_08_04 Update: v2.5 Added O/S distribution logo instead of name on page
 #@ 2019_08_18 Update: v2.6 Change page heading and some text fields.
+#@ 2019_09_23 Update: v2.7 Add Distribution logo and Version for each servers.
 # ==================================================================================================
 # REQUIREMENT COMMON TO ALL PAGE OF SADMIN SITE
 require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmInit.php');           # Load sadmin.cfg & Set Env.
@@ -64,7 +65,7 @@ require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmPageWrapper.php');    # Headin
 #===================================================================================================
 #
 $DEBUG          = False ;                                               # Debug Activated True/False
-$WVER           = "2.6" ;                                               # Current version number
+$WVER           = "2.7" ;                                               # Current version number
 $URL_CREATE     = '/crud/srv/sadm_server_create.php';                   # Create Page URL
 $URL_UPDATE     = '/crud/srv/sadm_server_update.php';                   # Update Page URL
 $URL_DELETE     = '/crud/srv/sadm_server_delete.php';                   # Delete Page URL
@@ -154,13 +155,7 @@ function display_data($count,$con,$row) {
     $WOS   = sadm_clean_data($row['srv_osname']);
     sadm_show_logo($WOS);                                               # Show Distribution Logo
 
-    # Display Server Alert
-    #echo "\n<td class='dt-center'>" ."None". "</td>";                  # Display Server Alert
-    
-    # Display O/S Name and O/S Version
-    #$os_name_ver = $row['srv_osname'] . "</a> " . $row['srv_osversion'];
-    #echo "\n<td class='dt-left'><a href='" . $URL_SERVER . "?selection=os";  
-    #echo "&value=" . $row['srv_osname']  ."'>"  . $os_name_ver . "</td>";
+    # Display O/S Version
     echo "\n<td class='dt-center'>" . $row['srv_osversion'] . "</td>";
 
     // # Display O/S Name and O/S Version
