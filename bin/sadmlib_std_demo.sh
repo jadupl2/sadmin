a95e8c43353b4e25c5536cce312f952f689b878b
Jacques Duplessis <duplessis.jacques@gmail.com>
2020-02-02 13:13:58 -0500

    Small Typo changes

--- bin/sadmlib_std_demo.sh
+++ bin/sadmlib_std_demo.sh
@@ -72,8 +72,8 @@ trap 'sadm_stop 0; exit 0' 2                                            # INTERC
 #---------------------------------------------------------------------------------------------------
 #
     # Default Value for these Global variables are defined in $SADMIN/cfg/sadmin.cfg file.
-    # But they can be overriden here on a per script basis.
-    #export SADM_ALERT_TYPE=1                           # 0=None 1=AlertOnErr 2=AlertOnOK 3=Allways
+    # But they can be overridden here on a per script basis.
+    export SADM_ALERT_TYPE=1                           # 0=None 1=AlertOnErr 2=AlertOnOK 3=Allways
     #export SADM_ALERT_GROUP="default"                  # AlertGroup Used to Alert (alert_group.cfg)
     #export SADM_MAIL_ADDR="your_email@domain.com"      # Email to send log (To Override sadmin.cfg)
     #export SADM_MAX_LOGLINE=1000                       # When Script End Trim log file to 1000 Lines
