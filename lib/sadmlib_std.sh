46dd5b80a06e216965c07015c8aafc91dd926a17
Jacques Duplessis <duplessis.jacques@gmail.com>
2020-04-05 10:12:10 -0400

    v3.34 New Variable SADM_SUCCESS, SADM_FAILED to Show Status in Color.

--- lib/sadmlib_std.sh
+++ lib/sadmlib_std.sh
@@ -132,6 +132,7 @@
 #@2020_04_01 Update: v3.31 Function sadm_writelog() with N/L depreciated, remplace by sadm_write().
 #@2020_04_01 Update: v3.32 Function sadm_writelog() replaced by sadm_write in Library.
 #@2020_04_04 Update: v3.33 New Variable SADM_OK, SADM_WARNING, SADM_ERROR to Show Status in Color.
+#@2020_04_05 Update: v3.34 New Variable SADM_SUCCESS, SADM_FAILED to Show Status in Color.
 #===================================================================================================
 trap 'exit 0' 2                                                         # Intercept The ^C
 #set -x
@@ -143,7 +144,7 @@ trap 'exit 0' 2                                                         # Interc
 # --------------------------------------------------------------------------------------------------
 #
 SADM_HOSTNAME=`hostname -s`                 ; export SADM_HOSTNAME      # Current Host name
-SADM_LIB_VER="3.33"                         ; export SADM_LIB_VER       # This Library Version
+SADM_LIB_VER="3.34"                         ; export SADM_LIB_VER       # This Library Version
 SADM_DASH=`printf %80s |tr " " "="`         ; export SADM_DASH          # 80 equals sign line
 SADM_FIFTY_DASH=`printf %50s |tr " " "="`   ; export SADM_FIFTY_DASH    # 50 equals sign line
 SADM_80_DASH=`printf %80s |tr " " "="`      ; export SADM_80_DASH       # 80 equals sign line
@@ -378,8 +379,10 @@ fi
 
 # Standard Variable to Show ERROR,OK,WARNING status uniformingly.
 export SADM_ERROR="[ $SADM_RED}ERROR${SADM_RESET} ]"                    # [ ERROR ] in Red
+export SADM_FAILED="[ $SADM_RED}FAILED${SADM_RESET} ]"                  # [ FAILED ] in Red
 export SADM_WARNING="[ ${SADM_BOLD}$SADM_YELLOW}WARNING${SADM_RESET} ]" # [ WARNING ] in Yellow
 export SADM_OK="[ ${SADM_BOLD}${SADM_GREEN}OK${SADM_RESET} ]"           # [ OK ] in Green
+export SADM_SUCCESS="[ ${SADM_BOLD}${SADM_GREEN}SUCCESS${SADM_RESET} ]" # [ SUCCESS ] in Green
 
 
 # --------------------------------------------------------------------------------------------------
