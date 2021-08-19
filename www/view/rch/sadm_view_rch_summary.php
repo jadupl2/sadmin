e86f7009471fdf908b250bb590b0cba6ff19da5b
Jacques Duplessis <duplessis.jacques@gmail.com>
2021-08-19 17:29:03 -0400

    2.7 Add link to Script documentation, no show alert group name & Members.

--- www/view/rch/sadm_view_rch_summary.php
+++ www/view/rch/sadm_view_rch_summary.php
@@ -32,13 +32,14 @@
 # 2019_06_07 Update: v2.4 Add Alarm type to page (Deal with new format).
 # 2019_09_20 Update v2.5 Show History (RCH) content using same uniform way.
 # 2020_12_13 Update v2.6 Add link in the heading to view the Daily Scripts Report, if HTML exist.
+#@2021_08_18 web 2.7 Add link to Script documentation, no show alert group name & Members.
 # ==================================================================================================
 # REQUIREMENT COMMON TO ALL PAGE OF SADMIN SITE
 require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmInit.php');           # Load sadmin.cfg & Set Env.
 require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmLib.php');            # Load PHP sadmin Library
 require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmPageHeader.php');     # <head>CSS,JavaScript</Head>
 require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmPageWrapper.php');    # Heading & SideBar
-
+ 
 # DataTable Initialisation Function
 ?>
 <script>
@@ -61,7 +62,7 @@ require_once ($_SERVER['DOCUMENT_ROOT'].'/lib/sadmPageWrapper.php');    # Headin
 #===================================================================================================
 #
 $DEBUG              = False ;                                           # Debug Activated True/False
-$SVER               = "2.6" ;                                           # Current version number
+$SVER               = "2.7" ;                                           # Current version number
 $CREATE_BUTTON      = False ;                                           # Yes Display Create Button
 $URL_HOST_INFO      = '/view/srv/sadm_view_server_info.php';            # Display Host Info URL
 $URL_VIEW_RCH       = '/view/rch/sadm_view_rchfile.php';                # View RCH File Content URL
@@ -70,6 +71,7 @@ $URL_REAR_REPORT    = "/view/daily_rear_report.html";                   # Rear D
 $URL_BACKUP_REPORT  = "/view/daily_backup_report.html";                 # Backup Daily Report Page
 $URL_STORIX_REPORT  = "/view/daily_storix_report.html";                 # Storix Daily Report Page
 $URL_SCRIPTS_REPORT = "/view/daily_scripts_report.html";                # Scripts Daily Report Page
+$URL_WEB            = "https://sadmin.ca/";                             # Main Site URL 
 
 
 # ==================================================================================================
@@ -85,32 +87,30 @@ function setup_table() {
     # PAGE TABLE HEADING 
     echo "\n<thead>\n";
     echo "<tr>\n";
-    echo "<th class='dt-head-left'>Server</th>\n";
+    echo "<th class='dt-head-left'>System</th>\n";
     echo "<th class='dt-left'>Script Name</th>\n";
-    echo "<th class='dt-center'>Start Date</th>\n";
-    echo "<th class='dt-center'>Start Time</th>\n";
-    echo "<th class='dt-head-center'>End Time</th>\n";
-    echo "<th class='dt-head-left'>Elapse</th>\n";
+    echo "<th class='dt-center'>Start Date/Time</th>\n";
+    echo "<th class='dt-center'>End Date/Time</th>\n";
+    echo "<th class='dt-center'>Elapse</th>\n";
     echo "<th class='dt-head-center'>Alert Group</th>\n";
     echo "<th class='dt-head-center'>Alert Type</th>\n";
     echo "<th class='dt-head-left'>Status</th>\n"; 
-    echo "<th class='text-center'>View Log / History</th>\n";
+    #echo "<th class='text-center'>Log&nbsp;&&nbsp;History</th>\n";
     echo "</tr>\n";
     echo "</thead>\n";
 
     # PAGE TABLE FOOTER
     echo "<tfoot>\n";
     echo "<tr>\n";
-    echo "<th class='dt-head-left'>Server</th>\n";
+    echo "<th class='dt-head-left'>System</th>\n";
     echo "<th class='dt-left'>Script Name</th>\n";
-    echo "<th class='dt-center'>Start Date</th>\n";
-    echo "<th class='dt-center'>Start Time</th>\n";
-    echo "<th class='dt-head-center'>End Time</th>\n";
-    echo "<th class='dt-head-left'>Elapse</th>\n";
+    echo "<th class='dt-center'>Start Date/Time</th>\n";
+    echo "<th class='dt-center'>End Date/Time</th>\n";
+    echo "<th class='dt-center'>Elapse</th>\n";
     echo "<th class='dt-head-center'>Alert Group</th>\n";
     echo "<th class='dt-head-center'>Alert Type</th>\n";
     echo "<th class='dt-head-left'>Status</th>\n"; 
-    echo "<th class='text-center'>View Log / History</th>\n";
+    #echo "<th class='text-center'>Log&nbsp;&&nbsp;History</th>\n";
     echo "</tr>\n";
     echo "</tfoot>\n";
 
@@ -124,7 +124,7 @@ function setup_table() {
 #   3rd Parameter is the Script Array 
 # ==================================================================================================
 function display_script_array($con,$wpage_type,$script_array) {
-    global $DEBUG, $URL_HOST_INFO, $URL_VIEW_RCH, $URL_VIEW_FILE ; 
+    global $DEBUG, $URL_HOST_INFO, $URL_VIEW_RCH, $URL_WEB, $URL_VIEW_FILE ; 
     
     # LOOP THROUGH THE SCRIPT ARRAY ----------------------------------------------------------------
     foreach($script_array as $key=>$value) { 
@@ -143,47 +143,160 @@ function display_script_array($con,$wpage_type,$script_array) {
             $row = mysqli_fetch_assoc($result);                         # Get Column Row Array 
             if ($row) { $wdesc   = $row['srv_desc']; } else { $wdesc   = "Unknown"; } #Get Srv Desc.
             
-            # DISPLAY SERVER NAME ------------------------------------------------------------------
+            # DISPLAY SERVER NAME
             echo "\n<tr>";
             echo "\n<td class='dt-left'>" ;
             echo "<a href='" . $URL_HOST_INFO . "?sel=" . $cserver . 
                  "' data-toggle='tooltip' title='" . $wdesc . "'>" . $cserver . "</a></td>";
+            
                  
-            # DISPLAY SCRIPT NAME, START DATE, START TIME, END TIME AND ELAPSE SCRIPT TIME ---------
-            echo "\n<td class='dt-left'>"   . $cname   . "</td>";       # Server Name Cell
-            echo "\n<td class='dt-center'>" . $cdate1  . "</td>";       # Start Date Cell
-            echo "\n<td class='dt-center'>" . $ctime1  . "</td>";       # Start Time Cell
+
+            # DISPLAY SCRIPT NAME
+            echo "\n<td class='dt-left'>"   . $cname ;                  # Server Name Cell
+
+            # Display links to access the log file (If exist)
+            $LOGFILE = trim("$cserver_${cname}.log");                         # Add .log to Script Name
+            $log_name = SADM_WWW_DAT_DIR . "/" . $cserver . "/log/" . $cserver . "_" . $LOGFILE ;
+            if (file_exists($log_name)) {
+                echo "\n<a href='" . $URL_VIEW_FILE . "?filename=" . 
+                $log_name . "' data-toggle='tooltip' title='View script log file'>&nbsp; [log]</a>";
+            }else{
+                echo "&nbsp;[No Log]";                                          # If No log exist for script
+            }
+ 
+            # Display links to access history (rch) file (If exist)
+            $RCHFILE = "$cserver_${cname}.rch";                         # Add .log to Script Name
+            $rch_name  = SADM_WWW_DAT_DIR . "/" . $cserver . "/rch/" . $cserver . "_" . trim($RCHFILE) ;
+            if (file_exists($rch_name)) {
+                echo "\n<a href='" . $URL_VIEW_RCH . "?host=". $cserver ."&filename=". $RCHFILE . 
+                   "' data-toggle='tooltip' title='View History (rch) file'>&nbsp; [rch]</a>";
+            }else{
+                echo "&nbsp;[No RCH]";                                         # If no RCH Exist
+            }
+            
+            # Display links to view script documentation (If exist)
+            $doc_link = getdocurl("$cname") ;                           # Get Script Name Link
+            if ( $doc_link != "" ) {                                    # We have a valid link ?
+                echo "\n<a href='" . $URL_WEB . $doc_link ;
+                echo "' title='View script documentation'>&nbsp; [doc]</a>";
+            }
+            
+            echo "</td>" ;
+
+
+
+
+            # DISPLAY START DATE, START TIME
+            echo "\n<td class='dt-center'>" . $cdate1  . "&nbsp;" . $ctime1 . "</td>";       # Start Date Cell
+
+            # DISPLAY END DATE, END TIME AND ELAPSE SCRIPT TIME
             if ($ccode == 2) {
                 echo "\n<td class='dt-center'>............</td>";       # Running - No End date Yet
-                echo "\n<td class='dt-center'>............</td>";       # Running - No Elapse time
+                echo "\n<td class='dt-center'>............</td>";       # Running - No End date Yet
             }else{
-                echo "\n<td class='dt-center'>" . $ctime2   . "</td>";  # Script Ending Time
-                echo "\n<td class='dt-left'>"   . $celapsed . "</td>";  # Script Elapse Time
+                echo "\n<td class='dt-center'>" . $cdate2 . "&nbsp;" . $ctime2 . "</td>";  # Script Ending Time
+                echo "\n<td class='dt-center'>" . $celapsed . "</td>";  # Script Elapse Time
             }
 
-            echo "\n<td class='dt-center'>" . $calert  . "</td>";       # Alert Group Name & Type
-            #echo "\n<td class='dt-center'>" . $calert  . "/" . $ctype . "</td>";       # Alert Group Name & Type
-          
-            # DISPLAY THE ALERT GROUP TYPE (0=None 1=AlertOnErr 2=AlertOnOK 3=Always)
-            echo "\n<td class='dt-center'>";
-            switch ($ctype) {
-                case 0:  
-                    echo "0 (No alert)</td>";
-                    break;
-                case 1:  
-                    echo "1 (Alert on error)</td>";
-                    break;
-                case 2:  
-                    echo "2 (Alert on Success)</td>";
-                    break;
-                case 3:  
-                    echo "3 (Always alert)</td>";
-                    break;
-                default: 
-                    echo "<font color='red'>Wront type " .$ctype. "</font></td>";
-                    break;;
+            # If 'default' alert group is used, get the real alert group name used.
+            if ($calert == "default") {                            # If Alert Group is default
+                $CMD="grep -i \"^default\" " . SADM_ALERT_FILE . " |awk '{print$3}' |tr -d ' '";
+                if ($DEBUG) { echo "\n<br>Command executed is : " . $CMD ; } 
+                unset($output_array);                                       # Clear output Array
+                exec ( $CMD , $output_array, $RCODE);                       # Execute command
+                if ($DEBUG) {                                               # If in Debug Mode
+                    echo "\n<br>Return code of command : " . $RCODE ;       # Command ReturnCode
+                    echo "\n<br>Content of output array:";                  # Show what's next
+                    echo '<code><pre>';                                     # Code to Show
+                    print_r($output_array);                                 # Show Cmd output
+                    echo '</pre></code>';                                   # End of code 
+                }
+                $calert=$output_array[0];                                   # Real Alert Grp Name
             }
-          
+
+            # Get the group Alert Type (M=Mail, S=SLack, T=Texto, C=Cellular)
+            $CMD="grep -i \"^" . $calert . "\" " . SADM_ALERT_FILE . " |awk '{print$2}' |tr -d ' '";
+            if ($DEBUG) { echo "\n<br>Command executed is : " . $CMD ;}     # Cmd we execute
+            unset($output_array);                                           # Clear output Array
+            exec ( $CMD , $output_array, $RCODE);                           # Execute command
+            if ($DEBUG) {                                                   # If in Debug Mode
+                echo "\n<br>Return code of command is : " . $RCODE ;        # Command ReturnCode
+                echo "\n<br>Content of output array :";                     # Show what's next
+                echo '<code><pre>';                                         # Code to Show
+                print_r($output_array);                                     # Show Cmd output
+                echo '</pre></code>';                                       # End of code 
+            }
+            $alert_group_type=$output_array[0];                             # GrpType t,m,s,c
+            if ($DEBUG) { echo "3) alert_group=..$alert_group.. alert_type=..$alert_type.."; }               
+
+            # Get Alert group members for tooltip
+            $CMD="grep -i \"^" . $calert . "\" " . SADM_ALERT_FILE . " |awk '{print$3}' |tr -d ' '";
+            if ($DEBUG) { echo "\n<br>Command executed is : " . $CMD ;}     # Cmd we execute
+            unset($output_array);                                           # Clear output Array
+            exec ( $CMD , $output_array, $RCODE);                           # Execute command
+            if ($DEBUG) {                                                   # If in Debug Mode
+                echo "\n<br>Return code of command is : " . $RCODE ;        # Command ReturnCode
+                echo "\n<br>Content of output array :";                     # Show what's next
+                echo '<code><pre>';                                         # Code to Show
+                print_r($output_array);                                     # Show Cmd output
+                echo '</pre></code>';                                       # End of code 
+            }
+            $alert_member=$output_array[0];                                 # Alert Group Member
+
+            # Build to tooltip for the alert group 
+            switch (strtoupper($alert_group_type)) {
+                case 'M' :  $stooltip="Alert send by email to " . $alert_member ;  # Alert Sent by Email
+                            break;
+                            ;; 
+                case 'C' :  $stooltip="Alert send by SMS to " . $alert_member ;    # Alert Sent by SMS
+                            break;
+                            ;; 
+                case 'T' :  $stooltip="Alert send by SMS to " . $alert_member ;    # Alert Sent by SMS
+                            break;
+                            ;; 
+                case 'S' :  $stooltip="Alert send with Slack to " . $alert_member ;# Alert Sent by SMS
+                            break;
+                            ;; 
+                default  :  $stooltip="Invalid Alert type(" . $alert_type .")";
+                            break;
+                            ;;
+            }                    
+
+            # Show Alert Group with Tooltip
+            echo "\n<td class='dt-center'>";
+            echo "<span data-toggle='tooltip' title='" . $stooltip . "'>"; 
+            echo $calert . "</span>(" . $alert_group_type . ")</td>"; 
+
+            # Display the alert group type (0=none, 1=alert onerror, 2=alert on ok, 3=always)
+
+            # Show Alert type Meaning
+            switch ($ctype) {                                                   # 0=No 1=Err 2=Success 3=All
+                case 0 :                                                        # 0=Don't send any Alert
+                    $alert_type_msg="No alert(0)" ;                             # Mess to show on page
+                    $etooltip="SADM_ALERT is to 0 in script " . $cname ;
+                    break;
+                case 1 :                                                        # 1=Send Alert on Error
+                    $alert_type_msg="Alert on error(1)" ;                   # Mess to show on page
+                    $etooltip="SADM_ALERT set to 1 in script " . $cname ;
+                    break;
+                case 2 :                                                        # 2=Send Alert on Success
+                    $alert_type_msg="Alert on success(2)" ;                     # Mess to show on page
+                    $etooltip="SADM_ALERT set to 2 in script " . $cname ;
+                    break;
+                case 3 :                                                        # 3=Always Send Alert
+                    $alert_type_msg="Always alert(3)" ;                         # Mess to show on page
+                    $etooltip="SADM_ALERT set to 3 in script " . $cname ;
+                    break;
+                default:
+                    $alert_type_msg="Unknown code($alert_type)" ;               # Invalid Alert Group Type
+                    $etooltip="SADM_ALERT set to ($alert_type) in script " . $cname ;
+                    break;
+            }        
+            echo "\n<td class='dt-center'>";
+            echo "<span data-toggle='tooltip' title='" . $etooltip . "'>"; 
+            echo $alert_type_msg . "</span></td>"; 
+
+    
             # DISPLAY THE SCRIPT STATUS BASED ON RETURN CODE ---------------------------------------
             echo "\n<td class='dt-left'><strong>";
             switch ($ccode) {
@@ -203,26 +316,27 @@ function display_script_array($con,$wpage_type,$script_array) {
             
             
             # DISPLAY LINKS TO ACCESS THE LOG FILE -------------------------------------------------
-            echo "\n<td class='dt-center'>" ;
-            list($fname,$fext) = explode ('.',$cfile);                  # Isolate Script Name
-            $LOGFILE = $fname . ".log";                                 # Add .log to Script Name
-            $log_name  = SADM_WWW_DAT_DIR . "/" . $row['srv_name'] . "/log/" . trim($LOGFILE) ;
-            if (file_exists($log_name)) {
-                echo "<a href='" . $URL_VIEW_FILE . "?filename=" . 
-                $log_name .  "' data-toggle='tooltip' title='View script log file.'>Log</a>&nbsp;&nbsp;&nbsp";
-            }else{
-                echo "No Log";                                          # If No log exist for script
-            }
+            #echo "\n<td class='dt-center'>" ;
+            #list($fname,$fext) = explode ('.',$cfile);                  # Isolate Script Name
+            #$LOGFILE = $fname . ".log";                                 # Add .log to Script Name
+            #$log_name  = SADM_WWW_DAT_DIR . "/" . $row['srv_name'] . "/log/" . trim($LOGFILE) ;
+            #if (file_exists($log_name)) {
+            #    echo "<a href='" . $URL_VIEW_FILE . "?filename=" . 
+            #    $log_name .  "' data-toggle='tooltip' title='View script log file.'>[log]</a>&nbsp;";
+            #}else{
+            #    echo "No Log";                                          # If No log exist for script
+            #}
  
             # DISPLAY LINKS TO ACCESS HISTORY FILE (RCH) -------------------------------------------
-            $rch_name  = SADM_WWW_DAT_DIR . "/" . $row['srv_name'] . "/rch/" . trim($cfile) ;
-            if (file_exists($rch_name)) {
-                echo "<a href='" . $URL_VIEW_RCH . "?host=". $cserver ."&filename=". $cfile . 
-                   "' data-toggle='tooltip' title='View History (rch) file.'>History</a>";
-            }else{
-                echo "No File";                                         # If no RCH Exist
-            }
-            echo "</td>" ;
+            #$rch_name  = SADM_WWW_DAT_DIR . "/" . $row['srv_name'] . "/rch/" . trim($cfile) ;
+            #if (file_exists($rch_name)) {
+            #    echo "<a href='" . $URL_VIEW_RCH . "?host=". $cserver ."&filename=". $cfile . 
+            #       "' data-toggle='tooltip' title='View History (rch) file.'>&nbsp;[rch]</a>";
+            #}else{
+            #    echo "No RCH";                                         # If no RCH Exist
+            #}
+            #echo "</td>" ;
+
             echo "\n</tr>\n";
         }
     }    
