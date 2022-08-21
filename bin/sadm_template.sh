f8057b7f8cb628ee6757847c91f36b9c5d3cbb39
Jacques Duplessis - sherlock <duplessis.jacques@gmail.com>
2022-08-21 11:57:19 -0400

    Remove newline in master function

--- bin/sadm_template.sh
+++ bin/sadm_template.sh
@@ -13,18 +13,9 @@
 # --------------------------------------------------------------------------------------------------
 #
 #   This code was originally written by Jacques Duplessis <sadmlinux@gmail.com>.
-#   Developer Web Site : https://sadmin.ca
-#
-#   The SADMIN Tool is free software; you can redistribute it and/or modify it under the terms
-#   of the GNU General Public License as published by the Free Software Foundation; either
-#   version 2 of the License, or (at your option) any later version.
-# 
-#   SADMIN Tools are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-#   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#   See the GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License along with this program.
-#   If not, see <http://www.gnu.org/licenses/>.
+#   Developer Web Site : h"${SADMIN}/tmp/${SADM_INST}_3.$$"    # Tmp File3 for you to use
+#export TMP_FILE1="$(mktemp ${SADMIN}/tmp/sadm_uninstall.XXXXXXXXX)"              # Temp File 1
+gnu.org/licenses/>.
 # 
 #---------------------------------------------------------------------------------------------------
 # Format on change log line : 
@@ -254,7 +245,7 @@ process_servers()
 #===================================================================================================
 main_process()
 {
-    sadm_write_log "Starting Main Process ...\n"                        # Starting processing Mess.
+    sadm_write_log "Starting Main Process ..."                          # Starting processing Mess.
     
     # PROCESSING CAN BE PUT HERE
     # If Error occurred, set SADM_EXIT_CODE to 1 before returning to caller, else return 0 (default)
