9f5275b5dd790e3d90a8a58d9d01613371e8ce32
Jacques Duplessis <Duplessis.jacques@gmail.com>
2024-04-23 10:36:42 -0400

    v2.5 Replace 'sadm_write' with 'sadm_write_log' and 'sadm_write_err'.

--- bin/sadm_backupdb.sh
+++ bin/sadm_backupdb.sh
@@ -40,6 +40,7 @@
 # 2020_04_01 backup v2.2 Replace function sadm_writelog() with N/L incl. by sadm_write() No N/L Incl.
 # 2021_05_26 backup v2.3 Added command line option option [-b backup_dir] [-n dbname].
 # 2022_08_17 backup v2.4 Updated with SADMIN section 1.52
+#@2024_04_17 backup v2.5 Replace 'sadm_write' with 'sadm_write_log' and 'sadm_write_err'.
 # --------------------------------------------------------------------------------------------------
 trap 'sadm_stop 0; exit 0' 2                                            # INTERCEPT The Control-C
 #set -x
@@ -170,29 +171,30 @@ show_usage()
 #===================================================================================================
 backup_setup()
 {
-    sadm_write "\n----------Setup Backup Environment ...\n"             # Advise User were Starting
+    sadm_write_log " "
+    sadm_write_log "----------Setup Backup Environment ..."             # Advise User were Starting
     
     which mysqldump >/dev/null 2>&1                                     # See if mysqldump available
     if [ $? -ne 0 ]                                                     # If Can't be found
-    then sadm_write_err "The 'mysqldump' command can't be found."   # Advise User
-        sadm_write_err "The Backup cannot be started."             # No Backup can be performed
-        return 1                                                   # Return Error to Caller
-    else                                                            # If mysqldump was found
-        MYSQLDUMP=`which mysqldump`                                 # Save mysqldump Full Path
+    then sadm_write_err "The 'mysqldump' command can't be found."       # Advise User
+         sadm_write_err "The Backup cannot be started."                 # No Backup can be performed
+         return 1                                                       # Return Error to Caller
+    else                                                                # If mysqldump was found
+         MYSQLDUMP=`which mysqldump`                                    # Save mysqldump Full Path
     fi
     
     # Main Backup Directory
     if [ ! -d "${BACKUP_ROOT_DIR}" ]                                    # Main Backup Dir. Exist ?
-    then mkdir $BACKUP_ROOT_DIR                                     # Create Directory
-        chown ${SADM_USER}:${SADM_GROUP} $BACKUP_ROOT_DIR          # Assign it SADM USer&Group
-        chmod 750 $BACKUP_ROOT_DIR                                 # Read/Write to SADM Usr/Grp
+    then mkdir $BACKUP_ROOT_DIR                                         # Create Directory
+         chown ${SADM_USER}:${SADM_GROUP} $BACKUP_ROOT_DIR              # Assign it SADM USer&Group
+         chmod 750 $BACKUP_ROOT_DIR                                     # Read/Write to SADM Usr/Grp
     fi
     
     # Daily Backup Directory
     if [ ! -d "${DAILY_DIR}" ]                                          # Daily Backup Dir. Exist ?
-    then mkdir $DAILY_DIR                                           # Create Directory
-        chown ${SADM_USER}:${SADM_GROUP} $DAILY_DIR                # Assign it SADM USer&Group
-        chmod 750 $DAILY_DIR                                       # Read/Write to SADM Usr/Grp
+    then mkdir $DAILY_DIR                                               # Create Directory
+         chown ${SADM_USER}:${SADM_GROUP} $DAILY_DIR                    # Assign it SADM USer&Group
+         chmod 750 $DAILY_DIR                                           # Read/Write to SADM Usr/Grp
     fi
     
     # Weekly Backup Directory
@@ -232,38 +234,37 @@ backup_setup()
     
     # If Database Name (-n) was specified, check if exist in Database
     if [ "$BACKUP_NAME" != 'all' ]                                      # If Backup one Database
-    then grep -i "$BACKUP_NAME" $SADM_TMP_FILE2 > /dev/null 2>&1    # Grep for DB Name in DBList
-        if [ $? -ne 0 ]                                            # If DB wasn't found
-        then sadm_write_err "The Database $BACKUP_NAME doesn't exist in Database.\n"
-            sadm_write_err "The valid Database name are : \n"  # SHow user List valid DB
-            while read dbname                                  # Read DB List one by one
-            do
-                sadm_write "${dbname}\n"                        # Display DB Name
-            done <$SADM_TMP_FILE2
-            return 1                                           # Return Error to Caller
-        else echo "$BACKUP_NAME" > $SADM_TMP_FILE2              # Put Name in List to Backup
+    then grep -i "$BACKUP_NAME" $SADM_TMP_FILE2 > /dev/null 2>&1        # Grep for DB Name in DBList
+         if [ $? -ne 0 ]                                                # If DB wasn't found
+            then sadm_write_err "The Database $BACKUP_NAME doesn't exist in Database.\n"
+                 sadm_write_err "The valid Database name are : \n"      # SHow user List valid DB
+                 while read dbname                                      # Read DB List one by one
+                    do
+                    sadm_write_log "${dbname}"                          # Display DB Name
+                    done <$SADM_TMP_FILE2
+            return 1                                                    # Return Error to Caller
+        else echo "$BACKUP_NAME" > $SADM_TMP_FILE2                      # Put Name in List to Backup
         fi
     fi
     
-    #sadm_write "You have chosen to : \n"
     if [ "$BACKUP_NAME" = 'all' ]
-    then sadm_write " - Backup All Databases.\n"
+    then sadm_write_log " - Backup All Databases."
         if [ "$DBEXCLUDE" != "" ]
-        then sadm_write "   - Except these databases ; $DBEXCLUDE \n"
+        then sadm_write_log "   - Except these databases ; $DBEXCLUDE "
         fi
-    else sadm_write " - Backup the Database '$BACKUP_NAME' \n"
+    else sadm_write_log " - Backup the Database '$BACKUP_NAME'"
     fi
     if [ "$COMPRESS_BACKUP" = 'Y' ]
-    then sadm_write " - Compress the backup file.\n"
-    else sadm_write " - Not compress the backup file.\n"
+    then sadm_write_log " - Compress the backup file."
+    else sadm_write_log " - Not compress the backup file."
     fi
-    sadm_write " - Keep $DAILY_BACKUP_TO_KEEP daily backups.\n"
-    sadm_write " - Keep $WEEKLY_BACKUP_TO_KEEP weekly backups.\n"
-    sadm_write " - Keep $MONTHLY_BACKUP_TO_KEEP monthly backups.\n"
-    sadm_write " - Keep $YEARLY_BACKUP_TO_KEEP yearly backups.\n"
-    sadm_write " - Do the weekly backup on ${WEEKDAY[$WEEKLY_BACKUP_DAY]}.\n"
-    sadm_write " - Do the monthly backup on the $MONTHLY_BACKUP_DATE of every month.\n"
-    sadm_write " - Do the yearly backup on the $YEARLY_BACKUP_DATE of ${MTH_NAME[$YEARLY_BACKUP_MONTH]} every year.\n"
+    sadm_write_log " - Keep $DAILY_BACKUP_TO_KEEP daily backups."
+    sadm_write_log " - Keep $WEEKLY_BACKUP_TO_KEEP weekly backups."
+    sadm_write_log " - Keep $MONTHLY_BACKUP_TO_KEEP monthly backups."
+    sadm_write_log " - Keep $YEARLY_BACKUP_TO_KEEP yearly backups."
+    sadm_write_log " - Do the weekly backup on ${WEEKDAY[$WEEKLY_BACKUP_DAY]}."
+    sadm_write_log " - Do the monthly backup on the $MONTHLY_BACKUP_DATE of every month."
+    sadm_write_log " - Do the yearly backup on the $YEARLY_BACKUP_DATE of ${MTH_NAME[$YEARLY_BACKUP_MONTH]} every year."
 }
 
 #===================================================================================================
@@ -274,36 +275,37 @@ backup_cleanup()
 {
     CLEAN_DIR=$1                                                        # Save Backup Dir. to Clean
     NB_KEEP=$2                                                          # Nb. OF Backup to Keep
-    sadm_write "\nCleaning `basename $CLEAN_DIR` Backup (Keep last $NB_KEEP Backups).\n"
+    sadm_write_log " "
+    sadm_write_log "Cleaning `basename $CLEAN_DIR` Backup (Keep last $NB_KEEP Backups)."
     
     # Produce a list of Databases Directories in $CLEAN_DIR
     find $CLEAN_DIR -type d -print |grep -v "${CLEAN_DIR}$" >${SADM_TMP_FILE1}
     if [ ! -s ${SADM_TMP_FILE1} ]                                       # Result file is Empty
-    then sadm_write "      - No Backup to delete (Currently 0).\n"   # No Backup done yet
-        return 0                                                    # Return Caller
+    then sadm_write_log "      - No Backup to delete (Currently 0)."    # No Backup done yet
+        return 0                                                        # Return Caller
     fi
     
     # Process all Databases Directories Found
     pwd_save=`pwd`                                                      # Save Current Directory
     while read dbdir                                                    # Read All DB Dir to process
     do
-        sadm_write "    - Pruning $dbdir Database Backup.\n"            # Show User Database pruning
+        sadm_write_log "    - Pruning $dbdir Database Backup."          # Show User Database pruning
         cd $dbdir                                                       # cd to Database Backup Dir.
-        if [ $SADM_DEBUG -gt 0 ] ; then ls -l ; fi                     # In Debug List Backup files
-        NB_BACKUP=`ls -1 | wc -l | tr -d ' '`                           # Count Nb. Backup in Dir.
-        NB_DELETE=`echo "$NB_BACKUP - $NB_KEEP" | $SADM_BC`             # Nb. Backup to Delete
-        if [ $SADM_DEBUG -gt 0 ]                                       # If Debug Activated
-        then sadm_write "NB_DELETE = $NB_BACKUP - $NB_KEEP \n"      # Show Math Done to Get Nb
+        if [ $SADM_DEBUG -gt 0 ] ; then ls -l ; fi                      # In Debug List Backup files
+        NB_BACKUP=$(ls -1 | wc -l | tr -d ' ')                          # Count Nb. Backup in Dir.
+        NB_DELETE=$(echo "$NB_BACKUP - $NB_KEEP" | $SADM_BC)            # Nb. Backup to Delete
+        if [ $SADM_DEBUG -gt 0 ]                                        # If Debug Activated
+        then sadm_write_log "NB_DELETE = $NB_BACKUP - $NB_KEEP  "       # Show Math Done to Get Nb
         fi
         if [ "$NB_DELETE" -gt 0 ]                                       # At Least 1 Backup to del.?
-        then sadm_write "      - $NB_DELETE Backup(s) out of $NB_BACKUP will be remove.\n"
+        then sadm_write_log "      - $NB_DELETE Backup(s) out of $NB_BACKUP will be remove."
             if [ $SADM_DEBUG -gt 0 ] ; then ls -1t | tail -${NB_DELETE} ;fi
-            ls -1t | tail -${NB_DELETE} | while read bname         # Process each Backup to Del
+            ls -1t | tail -${NB_DELETE} | while read bname              # Process each Backup to Del
             do
-                sadm_write "      - Deleting file $bname ...\n"     # Show Backup file to Del.
-                rm -f $bname                                        # Remove Database Backup
+                sadm_write_log "      - Deleting file $bname ..."       # Show Backup file to Del.
+                rm -f $bname                                            # Remove Database Backup
             done
-        else sadm_write "      - No Backup to delete (We have now $NB_BACKUP backup file(s)).\n"
+        else sadm_write_log "      - No Backup to delete (We have now $NB_BACKUP backup file(s))."
         fi
     done < ${SADM_TMP_FILE1}                                        # List of Database to clean
     cd $pwd_save                                                        # cd to previous save dir.
@@ -325,52 +327,54 @@ backup_db()
     then BACKUP_DIR="${WEEKLY_DIR}/$CURRENT_DB"                     # Will be Weekly Backup Dir.
     fi
     if [ "$CUR_DATE_NUM" -eq "$MONTHLY_BACKUP_DATE" ]                   # It's Monthly Backup Date ?
-    then BACKUP_DIR="${MONTHLY_DIR}/$CURRENT_DB"                    # Will be Monthly Backup Dir
+    then BACKUP_DIR="${MONTHLY_DIR}/$CURRENT_DB"                        # Will be Monthly Backup Dir
     fi
     if [ "$CUR_DATE_NUM" -eq "$YEARLY_BACKUP_DATE" ]                    # It's Year Backup Date ?
-    then if [ "$CUR_MTH_NUM" -eq "$YEARLY_BACKUP_MONTH" ]           # And It's Year Backup Mth ?
-        then BACKUP_DIR="${YEARLY_DIR}/$CURRENT_DB"             # Will be Yearly Backup Dir
+    then if [ "$CUR_MTH_NUM" -eq "$YEARLY_BACKUP_MONTH" ]               # And It's Year Backup Mth ?
+        then BACKUP_DIR="${YEARLY_DIR}/$CURRENT_DB"                     # Will be Yearly Backup Dir
         fi
     fi
     
     # Make Sure Backup Destination Directory Exist
     if [ ! -d "${BACKUP_DIR}" ]                                         # Backup Dir. Exist?
-    then mkdir $BACKUP_DIR                                          # Create Final Backup Dir.
-        chown ${SADM_USER}:${SADM_GROUP} $BACKUP_DIR               # Assign it SADM USer&Group
-        chmod 770 $BACKUP_DIR                                      # Read/Write to SADM Usr/Grp
+    then mkdir $BACKUP_DIR                                              # Create Final Backup Dir.
+        chown ${SADM_USER}:${SADM_GROUP} $BACKUP_DIR                    # Assign it SADM USer&Group
+        chmod 770 $BACKUP_DIR                                           # Read/Write to SADM Usr/Grp
     fi
     
-    sadm_write "\n----------\nStarting Backup of Database ${CURRENT_DB}.\n"  # Advise User were Starting
+    sadm_write_log " " 
+    sadm_write_log "----------" 
+    sadm_write_log "Starting Backup of Database ${CURRENT_DB}."         # Advise User were Starting
     BFILE="${BACKUP_DIR}/${BACKUP_FILENAME}"                            # Backup Filename Full Path
-    sadm_write "Backup Directory is ${BACKUP_DIR}\n"                    # Show User Backup Directory
+    sadm_write_log "Backup Directory is ${BACKUP_DIR}"                  # Show User Backup Directory
     if [ "$COMPRESS_BACKUP" = 'Y' ]                                     # If Compress option Chosen
-    then sadm_write "Backup FileName is ${BACKUP_FILENAME}.gz\n"    # Show Compress Backup Name
-    else sadm_write "Backup FileName is ${BACKUP_FILENAME}\n"       # Show User Backup Filename
+    then sadm_write_log "Backup FileName is ${BACKUP_FILENAME}.gz"      # Show Compress Backup Name
+    else sadm_write_log "Backup FileName is ${BACKUP_FILENAME}"         # Show User Backup Filename
     fi
     touch $BFILE                                                        # Create empty backup file
     chown ${SADM_USER}:${SADM_GROUP} $BFILE                             # Assign it SADM USer&Group
     chmod 640 $BFILE                                                    # Read/Write 2 SADM Usr Only
-    if [ $SADM_DEBUG -gt 5 ]                                           # If Debug Level > 5
-    then sadm_write "$MYSQLDUMP $CREDENTIAL $SADM_DBNAME \n"        # Debug = Write Command Used
+    if [ $SADM_DEBUG -gt 5 ]                                            # If Debug Level > 5
+        then sadm_write_log "$MYSQLDUMP $CREDENTIAL $SADM_DBNAME"       # Debug = Write Command Used
     fi
     
-    sadm_write "Running mysqldump ...\n"                                # Inform User
+    sadm_write_log "Running mysqldump ..."                              # Inform User
     $MYSQLDUMP $CREDENTIAL $SADM_DBNAME > $BFILE 2>&1                   # Run the Database Backup
     if [ $? -ne 0 ]                                                     # If Can't be found
-    then sadm_write_err "[ERROR] The 'mysqldump' command failed \n" # Advise User
-        return 1                                                   # Return Error to Caller
-    else sadm_write "[SUCCESS] Backup Succeeded \n"                 # Advise User
-        rm -f ${LATEST_DIR}/${CURRENT_DB}*                         # Remove Last DB Backup
-        if [ "$COMPRESS_BACKUP" = 'Y' ]                            # If Compress option Chosen
-        then gzip $BFILE                                        # Compress File
-            BFILE="${BFILE}.gz"                                # Add .gz to Backup Filename
-            cp ${BFILE} ${LATEST_DIR}                          # Copy Backup to Latest Dir.
-            chmod 440 ${LATEST_DIR}/${BACKUP_FILENAME}.gz      # Make file only Readable
-            chown ${SADM_USER}:${SADM_GROUP} ${LATEST_DIR}/${BACKUP_FILENAME}.gz
-        else cp ${BFILE} ${LATEST_DIR}                          # Copy Backup to Latest Dir.
-            chmod 440 ${LATEST_DIR}/${BACKUP_FILENAME}         # Make file only Readable
-            chown ${SADM_USER}:${SADM_GROUP} ${LATEST_DIR}/${BACKUP_FILENAME}
-        fi
+        then sadm_write_err "[ERROR] The 'mysqldump' command failed "   # Advise User
+             return 1                                                   # Return Error to Caller
+        else sadm_write_log "[SUCCESS] Backup Succeeded"                # Advise User
+             rm -f ${LATEST_DIR}/${CURRENT_DB}*                         # Remove Last DB Backup
+             if [ "$COMPRESS_BACKUP" = 'Y' ]                            # If Compress option Chosen
+                then gzip $BFILE                                        # Compress File
+                     BFILE="${BFILE}.gz"                                # Add .gz to Backup Filename
+                     cp ${BFILE} ${LATEST_DIR}                          # Copy Backup to Latest Dir.
+                     chmod 440 ${LATEST_DIR}/${BACKUP_FILENAME}.gz      # Make file only Readable
+                     chown ${SADM_USER}:${SADM_GROUP} ${LATEST_DIR}/${BACKUP_FILENAME}.gz
+                else cp ${BFILE} ${LATEST_DIR}                          # Copy Backup to Latest Dir.
+                     chmod 440 ${LATEST_DIR}/${BACKUP_FILENAME}         # Make file only Readable
+                     chown ${SADM_USER}:${SADM_GROUP} ${LATEST_DIR}/${BACKUP_FILENAME}
+             fi
     fi
 }
 
@@ -381,8 +385,8 @@ main_process()
 {
     # Make sure at least (-a or -n) we have a database backup to do.
     if [ "$BACKUP_NAME"  = "" ]                                         # No Database Name Specified
-    then sadm_write "[ERROR] No Database Name Specified.\n"
-        return 1
+        then sadm_write_err "[ERROR] No Database Name Specified."
+             return 1
     fi
     
     # Backup Directories
@@ -405,7 +409,8 @@ main_process()
     if [ "$ERROR_COUNT" -ne 0 ] ; then return 1 ; fi                    # If Error Return to Caller
     
     # Prune Backup Directories to respect the number of backup to keep, based on user choice -------
-    sadm_write "\n----------\n"                                         # Insert Sperator Dash Line
+    sadm_write_log " " 
+    sadm_write_log "----------"                                         # Insert Sperator Dash Line
     backup_cleanup "$DAILY_DIR"   "$DAILY_BACKUP_TO_KEEP"               # Purge unwanted Backup file
     if [ $? -ne 0 ] ; then return 1 ; fi                                # If Error Return to Caller
     backup_cleanup "$WEEKLY_DIR"  "$WEEKLY_BACKUP_TO_KEEP"              # Purge unwanted Backup file
