5dc5461cdd60045426840caa83426537b1a8a8d0
Jacques Duplessis <duplessis.jacques@gmail.com>
2025-08-25 11:30:50 -0400

    v3.13 Change constant 'SADM_PGM2DOC' to '$SADMIN/doc//sadm_commands/pgm2doc_link.cfg'

--- www/lib/sadmInit.php
+++ www/lib/sadmInit.php
@@ -31,6 +31,7 @@
 # 2021_09_15 web v3.10 Load new Var. SADM_MONITOR_RECENT_COUNT,SADM_MONITOR_RECENT_EXCLUDE
 # 2022_07_26 web v3.11 Set the TimeZone to America/Toronto
 # 2023_03_11 web v3.12 Load Rear backup diff & Interval at start, used on Rear Backup status page.
+#@2025_08_25 web v3.13 Change constant 'SADM_PGM2DOC' to '$SADMIN/doc//sadm_commands/pgm2doc_link.cfg'
 # --------------------------------------------------------------------------------------------------
 $DEBUG=False ;  
 #
@@ -38,7 +39,7 @@ $DEBUG=False ;
 
 
     # Set SADMIN PHP Library Version NUmber
-    define("SADM_PHP_LIBVER","3.12");
+    define("SADM_PHP_LIBVER","3.13");
 
     # Setting the HOSTNAME Variable
     list($HOSTNAME) = explode ('.', gethostname());                     # HOSTNAME without domain
@@ -116,7 +117,7 @@ define("SADM_WWW_NET_DIR"  , SADM_WWW_DAT_DIR . "/" .$HOSTNAME. "/net");    # We
 
 # SADMIN FILES DEFINITION
 define("SADM_CFG_FILE"            , SADM_CFG_DIR     . "/sadmin.cfg");          # SADMIN Config File
-define("SADM_PGM2DOC"             , SADM_WWW_DOC_DIR . "/pgm2doc_link.cfg");    # PGM to Doc link
+define("SADM_PGM2DOC"  ,SADM_WWW_DOC_DIR . "/sadm_commands/pgm2doc_link.cfg");    # PGM to Doc link
 define("SADM_BACKUP_LIST_INIT"    , SADM_CFG_DIR     . "/.backup_list.txt");    # BackupList Init
 define("SADM_BACKUP_EXCLUDE_INIT" , SADM_CFG_DIR     . "/.backup_exclude.txt"); # Backup Exclude Init
 define("SADM_REAR_EXCLUDE_INIT"   , SADM_CFG_DIR     . "/.rear_exclude.txt");   # ReaR Exclude Init
